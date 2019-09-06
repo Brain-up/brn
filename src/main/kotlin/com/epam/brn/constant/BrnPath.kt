70c63dbb2b6ca4150035647073e64c2dfc4d9e79
ElenaSpb <trunion@yandex.ru>
2019-09-06 20:49:34 +0300

    initial commit + add jpa, tests

--- src/main/kotlin/com/epam/brn/constant/BrnPath.kt
+++ src/main/kotlin/com/epam/brn/constant/BrnPath.kt
@@ -2,4 +2,5 @@ package com.lifescience.brn.constant
 
 object BrnPath {
     const val HOME = "/home"
+    const val USER = "/user"
 }
---
+++ src/test/kotlin/com/epam/brn/controller/UserDetailControllerTest.kt
@@ -0,0 +1,36 @@
+package com.epam.brn.controller
+
+import com.epam.brn.service.UserDetailsService
+import com.nhaarman.mockito_kotlin.times
+import com.nhaarman.mockito_kotlin.verify
+import org.junit.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.junit.jupiter.MockitoExtension
+
+@ExtendWith(MockitoExtension::class)
+internal class UserDetailControllerTest {
+
+    @InjectMocks
+    lateinit var userDetailController: UserDetailController
+
+    @Mock
+    lateinit var userDetailsService: UserDetailsService
+
+    @Test
+    fun `should insert user`() {
+        // GIVEN
+        val name = "Name"
+        val email = "email@email.ru"
+        val phone = "+7911111111"
+        `when`(userDetailsService.addUser(name, email, phone)).then { }
+
+        // WHEN
+        userDetailController.addUser(name, email, phone)
+
+        // THEN
+        verify(userDetailsService, times(1)).addUser(name, email, phone)
+    }
+}
