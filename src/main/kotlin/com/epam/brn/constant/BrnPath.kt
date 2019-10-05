d24eb06b3f987a5eb0d82fe2d256b2a6a09543e6
ElenaSpb <trunion@yandex.ru>
2019-10-05 10:38:44 +0300

    EPMLABSBRN-44 add alias

--- src/main/kotlin/com/epam/brn/constant/BrnPath.kt
+++ src/main/kotlin/com/epam/brn/constant/BrnPath.kt
@@ -3,5 +3,7 @@ package com.lifescience.brn.constant
 object BrnPath {
     const val HOME = "/home"
     const val USER = "/user"
+    const val SERIES = "/series"
+    const val EXERCISE = "/exercise"
     const val UI_VAADIN_ROUTE = "ui-vaadin" // without leading slash as per Vaadin convention
 }
