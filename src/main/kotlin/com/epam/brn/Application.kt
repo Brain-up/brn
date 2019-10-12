6f6ced2256c1f2c5868b5ae1795055333e0ea855
ElenaSpb <trunion@yandex.ru>
2019-10-12 14:23:07 +0300

    EPMLABSBRN-35 add swagger

--- src/main/kotlin/com/epam/brn/Application.kt
+++ src/main/kotlin/com/epam/brn/Application.kt
@@ -2,7 +2,9 @@ package com.epam.brn
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
+@EnableSwagger2
 @SpringBootApplication
 class Application
 
