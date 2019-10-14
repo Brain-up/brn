f47e59151e1035d506eecbd20468f1f86707727a
iki6 <39883772+iki6@users.noreply.github.com>
2019-10-14 21:18:32 +0300

    FE eslint, prettier, lint-stage, husky (#17)
    * prettier, lint-stages, husky
    
    * BRN-frontend precommit-settings adjustment
    
    * update paths & yarn.lock
    
    * added prettier config
    
    * made eslint respect prettier config
    
    * cleanup
    
    * added eslint to commit linting
    
    
    Co-authored-by: Irina Churkina <irina_churkina@epam.com>
    Co-authored-by: Artem Suschev <Suschev.Artem@gmail.com>

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -10,7 +10,8 @@ module.exports = {
   ],
   extends: [
     'eslint:recommended',
-    'plugin:ember/recommended'
+    'plugin:ember/recommended',
+    'prettier',
   ],
   env: {
     browser: true
