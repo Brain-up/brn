ea08b29e56d2235363dc5929b948cd3cbce48b78
Artem Suschev <Suschev.Artem@gmail.com>
2019-10-16 14:35:58 +0300

    switched to JSONAPI for demostration

--- frontend/app/routes/series/exercise.js
+++ frontend/app/routes/series/exercise.js
@@ -4,14 +4,16 @@ import { inject } from '@ember/service';
 export default Route.extend({
   store: inject(),
 
-  model({exercise_id:exerciseId}) {
-    return this.store.queryRecord('exercise',exerciseId);
+  model({ exercise_id }) {
+    return this.store.findRecord('exercise', exercise_id, { include: 'tasks' });
   },
 
-  async afterModel(exercise,{to}) {
-    await this.store.query('task',{exerciseId:exercise.id});
+  async afterModel(exercise, { to }) {
+    // in case if exercise was pre-loaded
+    // asking for tasks again
+    await exercise.hasMany('tasks').load();
     if (to.name.endsWith('exercise.index') && exercise.tasks.firstObject) {
-      this.transitionTo('series.exercise.task', exercise.tasks.firstObject)
+      this.transitionTo('series.exercise.task', exercise.tasks.firstObject);
     }
-  }
+  },
 });
