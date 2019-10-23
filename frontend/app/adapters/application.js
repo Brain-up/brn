c22a8d5f86c02c36a51dc3aa52a710eac9e7a9f7
Artem Suschev <Suschev.Artem@gmail.com>
2019-10-23 12:35:01 +0300

    switched to JSONAPI for demostration (#9)

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -1,4 +1,7 @@
 import DS from 'ember-data';
 
-export default DS.RESTAdapter.extend({
+export default DS.JSONAPIAdapter.extend({
+  coalesceFindRequests: true,
+  shouldReloadRecord: () => false,
+  shouldBackgroundReloadRecord: () => false,
 });
--- frontend/app/routes/series.js
+++ frontend/app/routes/series.js
@@ -4,11 +4,16 @@ import { inject } from '@ember/service';
 export default Route.extend({
   store: inject(),
 
-  async afterModel(series, {to}) {
-    await this.store.query('exercise',{});
-    if (to.name === 'series.index' && series.exercises.firstObject) {
-      this.transitionTo('series.exercise', series.exercises.firstObject)
-    }
+  model({ series_id }) {
+    return this.store.findRecord('series', series_id, { include: 'exercises' });
   },
 
+  async afterModel(series, { to }) {
+    // in case if series was pre-loaded
+    // asking for exercises again
+    await series.hasMany('exercises').load();
+    if (to.name === 'series.index' && series.exercises.firstObject) {
+      this.transitionTo('series.exercise', series.exercises.firstObject);
+    }
+  },
 });
