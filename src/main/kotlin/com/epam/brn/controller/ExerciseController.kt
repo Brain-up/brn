b2f4a115689356c3202dd86a3444823bcbffdf69
rubenpeciv <a@a.a>
2019-10-29 13:48:59 +0300

    EPMLABSBRN-96 Backend: remake end-point get available exercises for user as described in contract

--- src/main/kotlin/com/epam/brn/controller/ExerciseController.kt
+++ src/main/kotlin/com/epam/brn/controller/ExerciseController.kt
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import java.util.*
+import java.util.Collections
 
 @RestController
 @RequestMapping(BrnPath.EXERCISES)
--- src/main/kotlin/com/epam/brn/repo/ExerciseRepository.kt
+++ src/main/kotlin/com/epam/brn/repo/ExerciseRepository.kt
@@ -3,7 +3,6 @@ package com.epam.brn.repo
 import com.epam.brn.model.Exercise
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
-import java.util.*
 
 @Repository
 interface ExerciseRepository : CrudRepository<Exercise, Long> {
