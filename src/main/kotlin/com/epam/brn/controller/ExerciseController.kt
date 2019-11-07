409ee9c93a50f071de149a843ad590a42416554f
rubenpeciv <a@a.a>
2019-11-07 15:51:44 +0300

    EPMLABSBRN-101 Add availability on exercises retrieved on GET /exercises

--- src/main/kotlin/com/epam/brn/controller/ExerciseController.kt
+++ src/main/kotlin/com/epam/brn/controller/ExerciseController.kt
@@ -25,7 +25,7 @@ class ExerciseController(@Autowired val exerciseService: ExerciseService) {
     @ApiOperation("Get done exercises for user")
     fun getExercises(
         @RequestParam(value = USER_ID, required = true) userId: Long,
-        @RequestParam(value = SERIES_ID, required = false) seriesId: Long?
+        @RequestParam(value = SERIES_ID, required = true) seriesId: Long?
     ): ResponseEntity<BaseResponseDto> {
         seriesId?.let {
             return ResponseEntity.ok()
