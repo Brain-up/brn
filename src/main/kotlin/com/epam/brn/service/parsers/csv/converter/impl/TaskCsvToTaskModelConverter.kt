f904c736ebad6f4bf40d8bc4da87f7d7b3fb1242
Daria Sovkova <Daria_Sovkova@epam.com>
2019-11-13 19:16:59 +0300

    [EPMLABSBRN-129] - fix audio_file_url setting in load job

--- src/main/kotlin/com/epam/brn/service/parsers/csv/converter/impl/TaskCsvToTaskModelConverter.kt
+++ src/main/kotlin/com/epam/brn/service/parsers/csv/converter/impl/TaskCsvToTaskModelConverter.kt
@@ -1,6 +1,5 @@
 package com.epam.brn.service.parsers.csv.converter.impl
 
-import com.epam.brn.exception.EntityNotFoundException
 import com.epam.brn.model.Resource
 import com.epam.brn.model.Task
 import com.epam.brn.service.ExerciseService
@@ -10,10 +9,14 @@ import com.epam.brn.service.parsers.csv.dto.TaskCsv
 import org.apache.commons.collections4.CollectionUtils
 import org.apache.commons.lang3.StringUtils
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 @Component
 class TaskCsvToTaskModelConverter : Converter<TaskCsv, Task> {
+
+    @Value(value = "\${brn.audio.file.default.path}")
+    private lateinit var defaultAudioFileUrl: String
 
     @Autowired
     lateinit var exerciseService: ExerciseService
@@ -46,7 +49,7 @@ class TaskCsvToTaskModelConverter : Converter<TaskCsv, Task> {
         val resources = resourceService.findByWordAndAudioFileUrlLike(word, fileName)
 
         if (CollectionUtils.isEmpty(resources)) {
-            target.correctAnswer = createResource(word, fileName)
+            target.correctAnswer = createAndGetResource(word, fileName)
         } else {
             target.correctAnswer = resources[0]
         }
@@ -55,24 +58,30 @@ class TaskCsvToTaskModelConverter : Converter<TaskCsv, Task> {
     private fun convertAnswers(source: TaskCsv, target: Task) {
         target.answerOptions = CollectionUtils.emptyIfNull(source.words)
             .filter { StringUtils.isNotEmpty(it) }
-            .map { word -> getResourceByWord(word.replace("[()]".toRegex(), ""), source.fileName) }
+            .map {word -> word.replace("[()]".toRegex(), "")}
+            .map(this::getResourceByWord)
             .toMutableSet()
     }
 
-    private fun getResourceByWord(word: String, fileName: String): Resource {
+    private fun getResourceByWord(word: String): Resource {
         val resources = resourceService.findByWordLike(word)
 
         if (CollectionUtils.isEmpty(resources)) {
-            return createResource(word, fileName)
+            return createAndGetResource(word, StringUtils.EMPTY)
         }
 
         return resources[0]
     }
 
-    private fun createResource(word: String, fileName: String): Resource {
+    private fun createAndGetResource(word: String, fileName: String): Resource {
         val resource = Resource()
         resource.word = word
-        resource.audioFileUrl = fileName
+
+        if (StringUtils.isNotEmpty(fileName)) {
+            resource.audioFileUrl = fileName
+        } else {
+            resource.audioFileUrl = defaultAudioFileUrl.format(word)
+        }
 
         resourceService.save(resource)
 
