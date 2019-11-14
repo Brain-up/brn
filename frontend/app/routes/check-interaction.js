1726c5206410c308508165b6669327554a0a3d2c
Ирина Чуркина <irinachurkina@Irinas-MacBook-Pro.local>
2019-11-14 15:48:37 +0300

    Revert "EPMLABSBRN-122"
    This reverts commit 165564f5486956441fef39c838ef2c006b3add4c.

--- frontend/app/routes/check-interaction.js
+++ frontend/app/routes/check-interaction.js
@@ -1,15 +1,8 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  async afterModel(model, { to }) {
-    const parentContent = model.parent.content;
-    const parentModelName = parentContent.constructor.modelName;
-    const parentRouteId = to.parent.params[`${parentModelName}_id`];
-    const wrongParent =
-      parentRouteId &&
-      parentContent.id !== to.parent.params[`${parentModelName}_id`];
-
-    if (!model.canInteract || (parentModelName !== 'group' && wrongParent)) {
+  async afterModel(model) {
+    if (!model.canInteract) {
       this.transitionTo('not-accessable');
     }
   },
