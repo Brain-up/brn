165564f5486956441fef39c838ef2c006b3add4c
Ирина Чуркина <irinachurkina@Irinas-MacBook-Pro.local>
2019-11-14 14:09:54 +0300

    EPMLABSBRN-122
    check if the parent route is wrong

--- frontend/app/routes/check-interaction.js
+++ frontend/app/routes/check-interaction.js
@@ -1,8 +1,15 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  async afterModel(model) {
-    if (!model.canInteract) {
+  async afterModel(model, { to }) {
+    const parentContent = model.parent.content;
+    const parentModelName = parentContent.constructor.modelName;
+    const parentRouteId = to.parent.params[`${parentModelName}_id`];
+    const wrongParent =
+      parentRouteId &&
+      parentContent.id !== to.parent.params[`${parentModelName}_id`];
+
+    if (!model.canInteract || (parentModelName !== 'group' && wrongParent)) {
       this.transitionTo('not-accessable');
     }
   },
