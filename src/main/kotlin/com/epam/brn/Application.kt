ee7a28de0cfa2a2f43d1ff8e3606c2c7514d2a42
ElenaSpb <trunion@yandex.ru>
2019-11-18 20:01:56 +0300

    EPMLABSBRN-78 add spring cache - caffeine

--- src/main/kotlin/com/epam/brn/Application.kt
+++ src/main/kotlin/com/epam/brn/Application.kt
@@ -2,12 +2,14 @@ package com.epam.brn
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cache.annotation.EnableCaching
 import org.springframework.scheduling.annotation.EnableScheduling
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @EnableSwagger2
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 class Application
 
 fun main(args: Array<String>) {
--- src/main/kotlin/com/epam/brn/service/SeriesService.kt
+++ src/main/kotlin/com/epam/brn/service/SeriesService.kt
@@ -4,19 +4,24 @@ import com.epam.brn.dto.SeriesDto
 import com.epam.brn.exception.NoDataFoundException
 import com.epam.brn.repo.SeriesRepository
 import org.apache.logging.log4j.kotlin.logger
+import org.springframework.cache.annotation.CacheConfig
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 
 @Service
+@CacheConfig(cacheNames = arrayOf("seriesById", "seriesByGroupId"))
 class SeriesService(private val seriesRepository: SeriesRepository) {
 
     private val log = logger()
 
+    @Cacheable("seriesByGroupId")
     fun findSeriesForGroup(groupId: Long): List<SeriesDto> {
         log.debug("try to find series for groupId=$groupId")
         val series = seriesRepository.findByExerciseGroupLike(groupId)
         return series.map { seriesEntry -> seriesEntry.toDto() }
     }
 
+    @Cacheable("seriesById")
     fun findSeriesForId(seriesId: Long): SeriesDto {
         log.debug("try to find series for seriesId=$seriesId")
         val series = seriesRepository.findById(seriesId)
