2e8e3d9c33185b02effe5cb3a3efb56f16b01ddb
Daria Sovkova <Daria_Sovkova@epam.com>
2019-12-02 18:48:05 +0300

    [EPMLABSBRN-133] - PUT files end-point: add to response body all files lines which which was not added to db

--- src/main/kotlin/com/epam/brn/job/csv/task/impl/UploadTaskFromCsvJob.kt
+++ src/main/kotlin/com/epam/brn/job/csv/task/impl/UploadTaskFromCsvJob.kt
@@ -4,10 +4,12 @@ import com.epam.brn.constant.BrnErrors.CSV_FILE_FORMAT_ERROR
 import com.epam.brn.exception.FileFormatException
 import com.epam.brn.job.CsvUtils
 import com.epam.brn.job.csv.task.UploadFromCsvJob
+import com.epam.brn.model.Task
 import com.epam.brn.service.TaskService
 import com.epam.brn.service.parsers.csv.CSVParserService
 import com.epam.brn.service.parsers.csv.converter.impl.TaskCsvToTaskModelConverter
 import org.apache.commons.lang3.StringUtils
+import org.apache.logging.log4j.kotlin.logger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
@@ -19,29 +21,54 @@ import java.io.InputStream
 @Component
 class UploadTaskFromCsvJob(private val csvParserService: CSVParserService, private val taskService: TaskService) :
     UploadFromCsvJob {
+    private val log = logger()
 
     @Autowired
     private lateinit var taskCsvToTaskModelConverter: TaskCsvToTaskModelConverter
 
     @Throws(FileFormatException::class)
-    override fun uploadTasks(file: MultipartFile) {
+    override fun uploadTasks(file: MultipartFile): Map<String, String> {
         if (!isFileContentTypeCsv(file.contentType ?: StringUtils.EMPTY)) throw FileFormatException(CSV_FILE_FORMAT_ERROR)
 
-        uploadTasks(file.inputStream)
+        return uploadTasks(file.inputStream)
     }
 
     @Throws(FileFormatException::class)
-    override fun uploadTasks(file: File) {
-        uploadTasks(file.inputStream())
+    override fun uploadTasks(file: File): Map<String, String> {
+        return uploadTasks(file.inputStream())
     }
 
-    @Transactional
-    private fun uploadTasks(inputStream: InputStream) {
+    private fun uploadTasks(inputStream: InputStream): Map<String, String> {
         val tasks = csvParserService.parseCsvFile(inputStream, taskCsvToTaskModelConverter)
-        tasks.forEach { taskService.save(it) }
+
+        return saveTasks(tasks)
     }
 
     private fun isFileContentTypeCsv(contentType: String): Boolean {
         return CsvUtils.isFileContentTypeCsv(contentType)
+    }
+
+    private fun saveTasks(tasks: Map<String, Pair<Task?, String?>>): Map<String, String> {
+        val notSavingTasks = mutableMapOf<String, String>()
+
+        tasks.forEach {
+            val key = it.key
+            val task = it.value.first
+
+            try {
+                if (task != null) {
+                    taskService.save(task)
+                } else {
+                    it.value.second?.let { errorMessage -> notSavingTasks[key] = errorMessage }
+                }
+            } catch (e: Exception) {
+                notSavingTasks[key] = e.localizedMessage
+                log.warn("Failed to insert : $key ", e)
+            }
+
+            log.debug("Successfully inserted line: $key")
+        }
+
+        return notSavingTasks
     }
 }
