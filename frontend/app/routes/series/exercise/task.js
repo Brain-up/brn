690bbe695870d015a37c125ef38d8a7d10a38aa4
Irina Churkina <irina_churkina@epam.com>
2019-12-09 13:06:30 +0300

    fix tasks markers' motions

--- frontend/app/routes/series/exercise/task.js
+++ frontend/app/routes/series/exercise/task.js
@@ -1,18 +1,23 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  afterModel(task, { to }) {
+  async afterModel(task, { to }) {
     if (
       !task.canInteract ||
       (to.parent.params.exercise_id &&
         to.parent.params.exercise_id !== task.exercise.content.id)
     ) {
-      const exercise = task.get('exercise');
+      const exercise = await this.store.findRecord(
+        'exercise',
+        to.parent.params.exercise_id,
+      );
       const series = exercise.get('series');
+      const firstTask = exercise.get('sortedTasks.firstObject');
       this.transitionTo(
         'series.exercise.task',
         series.get('id'),
         exercise.get('id'),
+        firstTask.get('id'),
       );
     }
   },
