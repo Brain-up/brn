83f102ffb3878375788342caee882ca4ba15a9e1
Irina Churkina <irina_churkina@epam.com>
2019-12-09 13:41:56 +0300

    tests adjustments

--- frontend/app/routes/series/exercise/task.js
+++ frontend/app/routes/series/exercise/task.js
@@ -1,24 +1,24 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  async afterModel(task, { to }) {
+  afterModel(task, { to }) {
     if (
       !task.canInteract ||
       (to.parent.params.exercise_id &&
         to.parent.params.exercise_id !== task.exercise.content.id)
     ) {
-      const exercise = await this.store.findRecord(
-        'exercise',
-        to.parent.params.exercise_id,
-      );
-      const series = exercise.get('series');
-      const firstTask = exercise.get('sortedTasks.firstObject');
-      this.transitionTo(
-        'series.exercise.task',
-        series.get('id'),
-        exercise.get('id'),
-        firstTask.get('id'),
-      );
+      this.store
+        .findRecord('exercise', to.parent.params.exercise_id)
+        .then((exercise) => {
+          const series = exercise.get('series');
+          const firstTask = exercise.get('sortedTasks.firstObject');
+          this.transitionTo(
+            'series.exercise.task',
+            series.get('id'),
+            exercise.get('id'),
+            firstTask.get('id'),
+          );
+        });
     }
   },
 });
