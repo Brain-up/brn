1834326853125828125bff575d984850d5aa32a7
Artem Suschev <Suschev.Artem@gmail.com>
2019-12-19 14:34:07 +0300

    added stub Auth header EPMLABSBRN-166

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -1,8 +1,14 @@
 import DS from 'ember-data';
+import { computed } from '@ember/object';
 
 export default DS.RESTAdapter.extend({
   namespace: 'api',
   coalesceFindRequests: false,
   shouldReloadRecord: () => false,
   shouldBackgroundReloadRecord: () => false,
+  headers: computed(function() {
+    return {
+      Authorization: 'Basic dfkldklfsdjfkshfksghf;ldjsklghklsjfsdfjdsf;sdf',
+    };
+  }),
 });
