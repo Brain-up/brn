65376f0cb9d4c9a8cbb7325ff3b7b01db9fa5983
Ирина Чуркина <irinachurkina@EPRUMOSL7433.moscow.epam.com>
2019-12-30 11:00:35 +0300

    revert shuffle array loop

--- frontend/app/utils/shuffle-array.js
+++ frontend/app/utils/shuffle-array.js
@@ -1,10 +1,8 @@
-import deepEqual from 'brn/utils/deep-equal';
-
 export default function shuffleArray(a) {
   const shuffled = [...a];
   for (let i = a.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
   }
-  return deepEqual(shuffled, a) ? shuffleArray(a) : shuffled;
+  return shuffled;
 }
