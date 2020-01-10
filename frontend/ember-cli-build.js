be7d237e22c2f0805097a85700339784203401e2
Artem Suschev <Suschev.Artem@gmail.com>
2020-01-10 16:15:52 +0300

    turned off test selector stripping for prod builds

--- frontend/ember-cli-build.js
+++ frontend/ember-cli-build.js
@@ -6,6 +6,9 @@ const tailwindcss = require('tailwindcss');
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
+    'ember-test-selectors': {
+      strip: false
+    },
     fingerprint: {
       exclude: ['pictures/'],
     },
