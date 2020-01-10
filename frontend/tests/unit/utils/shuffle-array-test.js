2825944de837bb502a8021d502641b63d82c51b3
Ирина Чуркина <irinachurkina@Irinas-MBP.petersburg.epam.com>
2020-01-10 10:40:47 +0300

    update util test

--- frontend/tests/unit/utils/shuffle-array-test.js
+++ frontend/tests/unit/utils/shuffle-array-test.js
@@ -4,7 +4,7 @@ import { module, test } from 'qunit';
 module('Unit | Utility | shuffleArray', function() {
   // Replace this with your real tests.
   test('it shuffles array', function(assert) {
-    const testArray = [1, 2, 3];
+    const testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     let result = shuffleArray(testArray);
     assert.notDeepEqual(result, testArray);
   });
