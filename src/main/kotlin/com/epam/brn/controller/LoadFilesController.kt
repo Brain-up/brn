8351ed9c99d15da90fb38643beb6cb21f6e73b71
ElenaSpb <trunion@yandex.ru>
2020-01-14 18:06:43 +0300

    EPMLABSBRN-107 fix url, small refactoring

--- src/main/kotlin/com/epam/brn/controller/LoadFilesController.kt
+++ src/main/kotlin/com/epam/brn/controller/LoadFilesController.kt
@@ -2,20 +2,17 @@ package com.epam.brn.controller
 
 import com.epam.brn.constant.BrnParams.SERIES_ID
 import com.epam.brn.constant.BrnParams.TASK_FILE
-import com.epam.brn.constant.BrnPath.FILES
 import com.epam.brn.constant.BrnPath.LOAD_TASKS_FILE
 import com.epam.brn.dto.BaseResponseDto
 import com.epam.brn.job.csv.task.UploadFromCsvService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 
 @RestController
-@RequestMapping(FILES)
 class LoadFilesController(private val uploadTaskFromCsvService: UploadFromCsvService) {
 
     @PostMapping(LOAD_TASKS_FILE)
