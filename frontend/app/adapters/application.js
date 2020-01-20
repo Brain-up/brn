16102121c1fb9e85b676d3efc3f59bf636747235
Artem Suschev <Suschev.Artem@gmail.com>
2020-01-20 16:29:07 +0300

    synced frontend/app/adapters/application.js

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -6,6 +6,16 @@ export default DS.RESTAdapter.extend({
   coalesceFindRequests: false,
   shouldReloadRecord: () => false,
   shouldBackgroundReloadRecord: () => false,
+  urlForFindRecord(id, modelName, snapshot) {
+    let actualModelName = modelName;
+    if (
+      modelName === 'task/single-words' ||
+      modelName === 'task/words-sequences'
+    ) {
+      actualModelName = 'tasks';
+    }
+    return this._super(id, actualModelName, snapshot);
+  },
   headers: computed(function() {
     return {
       Authorization: 'Basic YWRtaW46YWRtaW4=',
