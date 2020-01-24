8732dc74ede1e171becd3faf9cdab6febba5e744
Andrei Perevozchikov <goopnigoop@gmail.com>
2020-01-24 09:40:18 +0300

    EPMLABSBRN-225 - init

--- src/main/kotlin/com/epam/brn/model/UserAccount.kt
+++ src/main/kotlin/com/epam/brn/model/UserAccount.kt
@@ -9,6 +9,8 @@ import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
 import javax.persistence.JoinColumn
+import javax.persistence.JoinTable
+import javax.persistence.ManyToMany
 import javax.persistence.OneToMany
 import javax.persistence.OneToOne
 
