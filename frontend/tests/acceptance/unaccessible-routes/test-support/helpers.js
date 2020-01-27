b2579ee2797b894854432e214171918f4b775881
iki6 <39883772+iki6@users.noreply.github.com>
2020-01-27 15:19:06 +0300

    Epmlabsbrn-204 - exercises opening(becoming available) order (#206)
    * main task logic, initial
    
    * acceptance test, a bit of tests\' helpers rearrangements

--- frontend/tests/acceptance/unaccessible-routes/test-support/helpers.js
+++ frontend/tests/acceptance/unaccessible-routes/test-support/helpers.js
@@ -3,6 +3,7 @@ import {
   getExerciseScenarioData,
   getSeriesScenarioData,
 } from './data-storage';
+import { getServerResponses } from '../../general-helpers';
 
 export function getUnaccessibleTaskScenario() {
   const { tasks, exercises, series, groups } = getTaskScenarioData();
@@ -17,43 +18,4 @@ export function getUnaccessibleExerciseScenario() {
 export function getUnaccessibleSeriesScenario() {
   const { tasks, series, groups, exercises } = getSeriesScenarioData();
   return getServerResponses({ tasks, series, groups, exercises });
-}
-
-function getServerResponses({ tasks, series, groups, exercises }) {
-  /* eslint-disable no-undef */
-  server.get('groups', () => {
-    return { data: groups };
-  });
-
-  server.get('series', (schema, request) => {
-    const targetGroup = Number(request.queryParams.groupId);
-    const seriesIds = groups.findBy('id', targetGroup).series;
-    return { data: series.filter((s) => seriesIds.includes(s.id)) };
-  });
-
-  server.get('exercises', (schema, request) => {
-    const targetSeries = Number(request.queryParams.seriesId);
-    const exercisesIds = series.findBy('id', targetSeries).exercises;
-    return { data: exercises.filter((e) => exercisesIds.includes(e.id)) };
-  });
-
-  server.get('tasks', () => {
-    return { data: tasks };
-  });
-
-  server.get('tasks/:id', (schema, request) => {
-    return { data: tasks.findBy('id', Number(request.params.id)) };
-  });
-
-  server.get('groups/:id', (schema, request) => {
-    return { data: groups.findBy('id', Number(request.params.id)) };
-  });
-
-  server.get('series/:id', (schema, request) => {
-    return { data: series.findBy('id', Number(request.params.id)) };
-  });
-
-  server.get('exercises/:id', (schema, request) => {
-    return { data: exercises.findBy('id', Number(request.params.id)) };
-  });
 }
