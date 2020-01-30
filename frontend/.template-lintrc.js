75ce5c0b082d9f6bc91615d6bb708d3754eefcb4
Aleksandr Kanunnikov <lifeart92@gmail.com>
2020-01-30 09:43:07 +0300

    styleNamespace

--- frontend/.template-lintrc.js
+++ frontend/.template-lintrc.js
@@ -5,6 +5,6 @@ module.exports = {
   rules: {
     'self-closing-void-elements': false,
     'no-curly-component-invocation': { allow: ['group.name', 'not', 'html-safe', 'disablePlayButton'] },
-    'no-implicit-this': { allow: ['data-test-logo', 'data-test-series-navigation-list-link', 'data-test-group-link', 'data-test-series-navigation-list-link'] }
+    'no-implicit-this': { allow: ['styleNamespace', 'data-test-logo', 'data-test-series-navigation-list-link', 'data-test-group-link', 'data-test-series-navigation-list-link'] }
   }
 };
