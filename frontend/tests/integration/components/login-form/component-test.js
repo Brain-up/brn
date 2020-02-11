e86a236b6ebd882e006b3266e29aee983f9dd030
Aleksandr Kanunnikov <lifeart92@gmail.com>
2020-02-11 11:13:55 +0300

    EPMLABSBRN-15

---
+++ frontend/tests/integration/components/login-form/component-test.js
@@ -0,0 +1,67 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, fillIn, click } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+import Service from '@ember/service';
+
+module('Integration | Component | login-form', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`<LoginForm />`);
+
+    assert.dom('form').exists();
+    assert.dom('[data-test-submit-form]').hasTagName('button');
+    assert.dom('[name="username"]').hasAttribute('required');
+    assert.dom('[name="password"]').hasAttribute('required');
+    assert.dom('[data-test-form-warning]').doesNotExist();
+    
+  });
+
+  test('it showing warning on empty fields if edited', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`<LoginForm />`);
+    assert.dom('[data-test-form-warning]').doesNotExist();
+    
+    await fillIn('[name="username"]', 'a');
+    await fillIn('[name="password"]', 'b');
+    assert.dom('[data-test-form-warning]').doesNotExist();
+
+    await fillIn('[name="username"]', '');
+    await fillIn('[name="password"]', 'b');
+    assert.dom('[data-test-form-warning]').exists();
+
+    await fillIn('[name="username"]', 'a');
+    await fillIn('[name="password"]', '');
+    assert.dom('[data-test-form-warning]').exists();
+
+    await fillIn('[name="username"]', '');
+    await fillIn('[name="password"]', '');
+    assert.dom('[data-test-form-warning]').exists();
+    
+  });
+
+
+  test('login button works as expected', async function(assert) {
+    assert.expect(3);
+    class MockSession extends Service {
+      authenticate(type, login, password) {
+        assert.ok(type);
+        assert.ok(login);
+        assert.ok(password);
+      }
+    }
+    this.owner.register('service:session', MockSession);
+
+    await render(hbs`<LoginForm />`);
+    
+    await fillIn('[name="username"]', 'a');
+    await fillIn('[name="password"]', 'b');
+    await click('[data-test-submit-form]');
+  })
+});
