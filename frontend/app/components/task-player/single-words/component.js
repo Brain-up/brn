581f32a25b19bcc38b3dcd82fb4a45d9eab7023e
Ирина Чуркина <irinachurkina@Irinas-MacBook-Pro.local>
2020-02-11 08:34:42 +0300

    correctness frame for single words tasks

--- frontend/app/components/task-player/single-words/component.js
+++ frontend/app/components/task-player/single-words/component.js
@@ -11,8 +11,8 @@ export default class TaskPlayerComponent extends Component {
   shuffledWords = null;
   lastAnswer = null;
   exerciseResultIsVisible = false;
-  taskResultIsVisible = false;
   previousTaskWords = null;
+  showCorrectnessFrame = false;
 
   @service('audio') audio;
 
@@ -21,12 +21,6 @@ export default class TaskPlayerComponent extends Component {
     this.onRightAnswer();
   }).restartable())
   runNextTaskTimer;
-
-  @(task(function*() {
-    yield customTimeout(2000);
-    this.set('taskResultIsVisible', false);
-  }).drop())
-  showTaskResult;
 
   classNames = ['flex-1', 'flex', 'flex-col'];
 
@@ -38,8 +32,8 @@ export default class TaskPlayerComponent extends Component {
   didReceiveAttrs() {
     if (this.previousTaskWords !== this.task.words) {
       this.shuffle();
-      this.set('lastAnswer', null);
     }
+    this.set('lastAnswer', null);
     this.set('previousTaskWords', this.task.words);
     this.set('exerciseResultIsVisible', false);
   }
@@ -50,19 +44,26 @@ export default class TaskPlayerComponent extends Component {
   }
 
   @action
-  handleSubmit(word) {
+  async handleSubmit(word) {
+    this.set('showCorrectnessFrame', true);
     this.set('lastAnswer', word);
+    await customTimeout(1000);
+    this.set('showCorrectnessFrame', false);
     if (word !== this.task.word) {
-      const currentWordsOrder = Array.from(this.shuffledWords);
-      this.task.set('repetitionCount', this.task.repetitionCount + 1);
-      this.task.set('nextAttempt', true);
-      this.set('taskResultIsVisible', true);
-      while (deepEqual(currentWordsOrder, this.shuffledWords)) {
-        this.shuffle();
-      }
+      this.handleWrongAnswer();
     } else {
       this.task.set('nextAttempt', false);
     }
+  }
+
+  handleWrongAnswer() {
+    const currentWordsOrder = Array.from(this.shuffledWords);
+    this.task.set('repetitionCount', this.task.repetitionCount + 1);
+    this.task.set('nextAttempt', true);
+    while (deepEqual(currentWordsOrder, this.shuffledWords)) {
+      this.shuffle();
+    }
+    this.audio.player.playAudio();
   }
 }
 ({});
