7a541145e36657edf4c4242b11554dff5ee09d24
Joshua Oguma <joshua_oguma@epuakhaw0361.kyiv.epam.com>
2020-02-14 12:12:21 +0200

    EPMLABSBRN-171 - Implemented the upload file to S3 storage functionality

--- frontend-angular/src/app/admin/components/load-file/load-file.component.spec.ts
+++ frontend-angular/src/app/admin/components/load-file/load-file.component.spec.ts
@@ -18,8 +18,4 @@ describe('LoadFileComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
 });
