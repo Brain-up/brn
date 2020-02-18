b8072d5d080aa244d31bd2a0d2c2c2000e9c98d6
Elizaveta Tomaeva <elizaveta_tomaeva@epam.com>
2020-02-18 17:46:08 +0300

    EPMLABSBRN-215 test added

--- frontend/tests/integration/components/task-player/sentence/component-test.js
+++ frontend/tests/integration/components/task-player/sentence/component-test.js
@@ -1,13 +1,37 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+import data from './test-support/data-storage';
+import pageObject from './test-support/page-object';
 
 module('Integration | Component | task-player/sentence', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  hooks.beforeEach(async function() {
+    const store = this.owner.lookup('service:store');
+    this.set('model', store.createRecord('task/sentence'));
 
-    assert.equal(this.element.textContent.trim(), '');
+    this.model.setProperties(data.task);
+
+    store.createRecord('exercise', { tasks: [this.model] });
+
+    await render(hbs`<TaskPlayer
+      @task={{this.model}}
+    />`);
+  });
+
+  test('it shows all the words for scentence', async function(assert) {
+    const pageWords = pageObject.buttons.mapBy('word');
+
+    Object.values(data.task.answerOptions)
+      .reduce((array, subArray) => {
+        array = array.concat(subArray);
+        return array;
+      }, [])
+      .mapBy('word')
+      .forEach((word) => {
+        assert.ok(pageWords.includes(word), `word "${word}" is present`);
+      });
   });
 });
