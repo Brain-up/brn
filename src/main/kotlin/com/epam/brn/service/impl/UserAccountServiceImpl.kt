ce1327af40635a98e5c73cf28940165baaaedc6b
ElenaSpb <trunion@yandex.ru>
2020-02-26 12:33:43 +0300

    EPMLABSBRN-187 rest end-point for auth add

--- src/main/kotlin/com/epam/brn/service/impl/UserAccountServiceImpl.kt
+++ src/main/kotlin/com/epam/brn/service/impl/UserAccountServiceImpl.kt
@@ -11,6 +11,7 @@ import com.epam.brn.service.UserAccountService
 import java.security.Principal
 import org.apache.commons.lang3.StringUtils.isNotEmpty
 import org.apache.logging.log4j.kotlin.logger
+import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.UserDetails
@@ -38,6 +39,9 @@ class UserAccountServiceImpl(
     }
 
     override fun addUser(userAccountDto: UserAccountDto): UserAccountDto {
+        val existUser = userAccountRepository.findUserAccountByEmail(userAccountDto.email)
+        existUser.ifPresent { throw BadCredentialsException("User with email ${userAccountDto.email} is already exist!") }
+
         val setOfAuthorities = getTheAuthoritySet(userAccountDto)
         val hashedPassword = getHashedPassword(userAccountDto)
 
