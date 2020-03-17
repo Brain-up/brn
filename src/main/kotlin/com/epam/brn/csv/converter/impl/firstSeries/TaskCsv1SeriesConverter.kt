a00e7c7942fa23ecaabd667f074cb3cba0c5ef8f
Mihail Melnik <mikhailimelnik@gmail.com>
2020-03-17 16:40:03 +0200

    EPMLABSBRN-306 rework TaskCsv1SeriesConverter class to make it more readable <eom>

--- src/main/kotlin/com/epam/brn/csv/converter/impl/firstSeries/TaskCsv1SeriesConverter.kt
+++ src/main/kotlin/com/epam/brn/csv/converter/impl/firstSeries/TaskCsv1SeriesConverter.kt
@@ -4,21 +4,19 @@ import com.epam.brn.constant.WordTypeEnum
 import com.epam.brn.csv.converter.Converter
 import com.epam.brn.csv.dto.TaskCsv
 import com.epam.brn.exception.EntityNotFoundException
+import com.epam.brn.model.Exercise
 import com.epam.brn.model.Resource
 import com.epam.brn.model.Task
 import com.epam.brn.service.ExerciseService
 import com.epam.brn.service.ResourceService
 import org.apache.commons.collections4.CollectionUtils
 import org.apache.commons.lang3.StringUtils
-import org.apache.logging.log4j.kotlin.logger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 @Component
 class TaskCsv1SeriesConverter : Converter<TaskCsv, Task> {
-
-    private val log = logger()
 
     @Value(value = "\${brn.audio.file.default.path}")
     private lateinit var defaultAudioFileUrl: String
@@ -30,69 +28,66 @@ class TaskCsv1SeriesConverter : Converter<TaskCsv, Task> {
     lateinit var resourceService: ResourceService
 
     override fun convert(source: TaskCsv): Task {
-        val target = Task()
-        convertSerialNumber(source, target)
-        convertExercise(source, target)
-        convertCorrectAnswer(source, target)
-        convertAnswers(source, target)
-        return target
+        val result = Task()
+        result.serialNumber = source.orderNumber
+        result.exercise = prepareExercise(source)
+        result.correctAnswer = prepareCorrectAnswer(source)
+        result.answerOptions = prepareAnswerOptions(source.words)
+        return result
     }
 
-    private fun convertSerialNumber(source: TaskCsv, target: Task) {
-        target.serialNumber = source.orderNumber
-    }
-
-    private fun convertExercise(source: TaskCsv, target: Task) {
-        try {
-            target.exercise = exerciseService.findExerciseByNameAndLevel(source.exerciseName, source.level)
+    private fun prepareExercise(source: TaskCsv): Exercise {
+        return try {
+            exerciseService.findExerciseByNameAndLevel(source.exerciseName, source.level)
         } catch (e: EntityNotFoundException) {
-            log.debug("Entity was not found by name ${source.exerciseName}")
-            target.exercise = exerciseService.createExercise(source.exerciseName)
+            exerciseService.createExercise(source.exerciseName)
         }
     }
 
-    private fun convertCorrectAnswer(source: TaskCsv, target: Task) {
-        val word = source.word
-        val wordType = source.wordType
-        val audioFileName = source.audioFileName
-        val resources = resourceService.findByWordAndAudioFileUrlLike(word, audioFileName)
-        val correctAnswer: Resource
-        if (CollectionUtils.isEmpty(resources)) {
-            correctAnswer = createAndGetResource(word, audioFileName, source.pictureFileName, wordType)
+    private fun prepareCorrectAnswer(source: TaskCsv): Resource {
+        var resource = findFirstResourceBy(source.word, source.audioFileName)
+        if (resource != null) {
+            resource.wordType = source.wordType
+            resource.pictureFileUrl = source.pictureFileName
         } else {
-            correctAnswer = resources[0]
-            correctAnswer.pictureFileUrl = source.pictureFileName
-            correctAnswer.wordType = wordType
+            resource = Resource(
+                audioFileUrl = source.audioFileName,
+                word = source.word,
+                wordType = source.wordType,
+                pictureFileUrl = source.pictureFileName
+            )
         }
-        target.correctAnswer = resourceService.save(correctAnswer)
+        return resourceService.save(resource)
     }
 
-    private fun convertAnswers(source: TaskCsv, target: Task) {
-        target.answerOptions = CollectionUtils.emptyIfNull(source.words)
+    private fun findFirstResourceBy(word: String, audioFileName: String): Resource? {
+        val resources = resourceService.findByWordAndAudioFileUrlLike(word, audioFileName)
+        return if (resources.isNotEmpty()) resources.first() else null
+    }
+
+    private fun prepareAnswerOptions(words: List<String>): MutableSet<Resource> {
+        return CollectionUtils.emptyIfNull(words)
             .asSequence()
             .filter { StringUtils.isNotEmpty(it) }
             .map { word -> word.replace("[()]".toRegex(), "") }
-            .map(this::getResourceByWord)
-            .map(resourceService::save)
+            .map { this.toAnswerOption(it) }
             .toMutableSet()
     }
 
-    private fun getResourceByWord(word: String): Resource {
-        val resources = resourceService.findByWordLike(word)
-        return if (CollectionUtils.isEmpty(resources))
-            createAndGetResource(word, StringUtils.EMPTY, StringUtils.EMPTY, WordTypeEnum.UNKNOWN.toString())
-        else
-            resources.first()
+    private fun toAnswerOption(word: String): Resource {
+        return findFirstResourceBy(word)
+            ?: resourceService.save(
+                Resource(
+                    audioFileUrl = defaultAudioFileUrl.format(word),
+                    word = word,
+                    wordType = WordTypeEnum.UNKNOWN.toString(),
+                    pictureFileUrl = null
+                )
+            )
     }
 
-    private fun createAndGetResource(word: String, audioFileName: String, pictureFileName: String, wordType: String): Resource {
-        val resource = Resource()
-        resource.word = word
-        resource.wordType = WordTypeEnum.valueOf(wordType).toString()
-        resource.audioFileUrl =
-            if (StringUtils.isNotEmpty(audioFileName)) audioFileName else defaultAudioFileUrl.format(word)
-        resource.pictureFileUrl =
-            if (StringUtils.isNotEmpty(pictureFileName)) pictureFileName else null
-        return resource
+    private fun findFirstResourceBy(word: String): Resource? {
+        val resources = resourceService.findByWordLike(word)
+        return if (resources.isNotEmpty()) resources.first() else null
     }
 }
