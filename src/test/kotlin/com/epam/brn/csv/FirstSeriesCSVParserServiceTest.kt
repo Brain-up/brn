e2e14eccc51d05e75b0575f26fe17c36a1b58a1f
Mihail Melnik <mikhailimelnik@gmail.com>
2020-03-18 15:33:08 +0200

    EPMLABSBRN-306 rework FirstSeriesCSVParserServiceTest
    - rework FirstSeriesCSVParserServiceTest to use JUnit instead of spekframework
    - remove spekframework as we do not need it anymore

--- src/test/kotlin/com/epam/brn/csv/FirstSeriesCSVParserServiceTest.kt
+++ src/test/kotlin/com/epam/brn/csv/FirstSeriesCSVParserServiceTest.kt
@@ -10,97 +10,114 @@ import com.epam.brn.csv.firstSeries.commaSeparated.CommaSeparatedExerciseCSVPars
 import com.epam.brn.csv.firstSeries.commaSeparated.CommaSeparatedGroupCSVParserService
 import com.epam.brn.csv.firstSeries.commaSeparated.CommaSeparatedSeriesCSVParserService
 import java.nio.charset.StandardCharsets
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.specification.describe
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 
-private inline fun <reified T> makeIdentityConverter(): Converter<T, T> =
-    object : Converter<T, T> {
-        override fun convert(source: T) = source
+class FirstSeriesCSVParserServiceTest {
+
+    private val csvMappingIteratorParser = CsvMappingIteratorParser()
+
+    private val taskCsvParserService = TaskCSVParser1SeriesService()
+    private val exerciseCsvParserService = CommaSeparatedExerciseCSVParserService()
+    private val groupCsvParserService = CommaSeparatedGroupCSVParserService()
+    private val seriesCsvParserService = CommaSeparatedSeriesCSVParserService()
+
+    private inline fun <reified T> makeIdentityConverter(): Converter<T, T> {
+        return object : Converter<T, T> {
+            override fun convert(source: T) = source
+        }
     }
 
-val csvMappintIteratorParser = CsvMappingIteratorParser()
-
-val taskCSVParserService = TaskCSVParser1SeriesService()
-
-val commaSeparatedExerciseCSVParserService = CommaSeparatedExerciseCSVParserService()
-
-val commaSeparatedExerciseGroupCSVParserService = CommaSeparatedGroupCSVParserService()
-
-val commaSeparatedSeriesCSVParserService = CommaSeparatedSeriesCSVParserService()
-
-class FirstSeriesCSVParserServiceTest : Spek({
-    describe("Csv Parser Service") {
-        it("should parse Tasks") {
-
-            val input = """
+    @Test
+    fun `should parse Tasks`() {
+        val input = """
                 level exerciseName orderNumber word audioFileName pictureFileName words wordType
                 1 name1 1 бал no_noise/бал.mp3 pictures/бал.jpg (бам,сам,дам,зал,бак) OBJECT
                 2 name1 3 foo no_noise/foo.mp3 pictures/foo.jpg (foo,bar,baz) OBJECT
                 """.trimIndent()
 
-            val result = input.byteInputStream(StandardCharsets.UTF_8).use {
-                csvMappintIteratorParser.parseCsvFile(it, makeIdentityConverter(), taskCSVParserService)
-            }.map { res -> res.value.first }.toList()
+        val result = input.byteInputStream(StandardCharsets.UTF_8).use {
+            csvMappingIteratorParser.parseCsvFile(it, makeIdentityConverter(), taskCsvParserService)
+        }.map { res -> res.value.first }.toList()
 
-            result shouldContain TaskCsv(
-                1, "name1", 1, "бал", "no_noise/бал.mp3", "pictures/бал.jpg",
-                listOf("(бам", "сам", "дам", "зал", "бак)"), "OBJECT"
+        assertThat(result).containsAll(
+            listOf(
+                TaskCsv(
+                    1, "name1", 1,
+                    "бал", "no_noise/бал.mp3", "pictures/бал.jpg",
+                    listOf("(бам", "сам", "дам", "зал", "бак)"), "OBJECT"
+                ), TaskCsv(
+                    2, "name1", 3,
+                    "foo", "no_noise/foo.mp3", "pictures/foo.jpg",
+                    listOf("(foo", "bar", "baz)"), "OBJECT"
+                )
             )
+        )
+    }
 
-            result shouldContain TaskCsv(
-                2, "name1", 3, "foo", "no_noise/foo.mp3", "pictures/foo.jpg",
-                listOf("(foo", "bar", "baz)"), "OBJECT"
-            )
-        }
-
-        it("should parse Exercises") {
-
-            val input = """
+    @Test
+    fun `should parse Exercises`() {
+        val input = """
                 exerciseId, seriesId, level, name, description
                 1, 1, 1, Однослоговые слова без шума, Однослоговые слова без шума
                 2, 1, 2, Однослоговые слова без шума, Однослоговые слова без шума                
                 """.trimIndent()
 
-            val result = input.byteInputStream(StandardCharsets.UTF_8).use {
-                csvMappintIteratorParser.parseCsvFile(it, makeIdentityConverter(), commaSeparatedExerciseCSVParserService)
-            }.map { res -> res.value.first }.toList()
+        val result = input.byteInputStream(StandardCharsets.UTF_8).use {
+            csvMappingIteratorParser.parseCsvFile(it, makeIdentityConverter(), exerciseCsvParserService)
+        }.map { res -> res.value.first }.toList()
 
-            val name = "Однослоговые слова без шума"
-            result shouldBeEqualTo listOf(
-                ExerciseCsv(1, 1, 1, name, name), ExerciseCsv(2, 1, 2, name, name)
+        val name = "Однослоговые слова без шума"
+        assertThat(result).containsAll(
+            listOf(
+                ExerciseCsv(1, 1, 1, name, name),
+                ExerciseCsv(2, 1, 2, name, name)
             )
-        }
+        )
+    }
 
-        it("should parse Groups") {
+    @Test
+    fun `should parse Groups`() {
 
-            val input = """
+        val input = """
                 groupId, name, description
                 1, Неречевые упражнения, Неречевые упражнения
                 2, Речевые упражнения, Речевые упражнения              
                 """.trimIndent()
 
-            val result = input.byteInputStream(StandardCharsets.UTF_8).use {
-                csvMappintIteratorParser.parseCsvFile(it, makeIdentityConverter(), commaSeparatedExerciseGroupCSVParserService)
-            }.map { res -> res.value.first }.toList()
+        val result = input.byteInputStream(StandardCharsets.UTF_8).use {
+            csvMappingIteratorParser.parseCsvFile(
+                it,
+                makeIdentityConverter(),
+                groupCsvParserService
+            )
+        }.map { res -> res.value.first }.toList()
 
-            result shouldContain GroupCsv(1, "Неречевые упражнения", "Неречевые упражнения")
-            result shouldContain GroupCsv(2, "Речевые упражнения", "Речевые упражнения")
-        }
+        assertThat(result).containsAll(
+            listOf(
+                GroupCsv(1, "Неречевые упражнения", "Неречевые упражнения"),
+                GroupCsv(2, "Речевые упражнения", "Речевые упражнения")
+            )
+        )
+    }
 
-        it("should parse Series") {
-
-            val input = """
+    @Test
+    fun `should parse Series`() {
+        val input = """
                 groupId, seriesId, name, description
                 2, 1, Распознование слов, Распознование слов
                 2, 2, Составление предложений, Составление предложений         
                 """.trimIndent()
 
-            val result = input.byteInputStream(StandardCharsets.UTF_8).use {
-                csvMappintIteratorParser.parseCsvFile(it, makeIdentityConverter(), commaSeparatedSeriesCSVParserService)
-            }.map { res -> res.value.first }.toList()
+        val result = input.byteInputStream(StandardCharsets.UTF_8).use {
+            csvMappingIteratorParser.parseCsvFile(it, makeIdentityConverter(), seriesCsvParserService)
+        }.map { res -> res.value.first }.toList()
 
-            result shouldContain SeriesCsv(2, 1, "Распознование слов", "Распознование слов")
-            result shouldContain SeriesCsv(2, 2, "Составление предложений", "Составление предложений")
-        }
+        assertThat(result).containsAll(
+            listOf(
+                SeriesCsv(2, 1, "Распознование слов", "Распознование слов"),
+                SeriesCsv(2, 2, "Составление предложений", "Составление предложений")
+            )
+        )
     }
-})
+}
