9822bad3bece0885745a929d4f89263f9f7918cd
Elizaveta Tomaeva <elizaveta_tomaeva@epam.com>
2020-04-01 17:09:13 +0300

    EPMLABSBRN-270 fixes

--- frontend/app/controllers/application.js
+++ frontend/app/controllers/application.js
@@ -5,11 +5,6 @@ import { computed } from '@ember/object';
 export default Controller.extend({
   router: inject(),
   headerAndNavShown: computed('router.currentURL', function() {
-    console.log(
-      this.router.currentURL,
-      this.router.currentURL.includes('task'),
-      this.target.currentRouteName,
-    );
     return this.router.currentURL.includes('task');
   }),
 });
--- frontend/app/controllers/group.js
+++ frontend/app/controllers/group.js
@@ -5,10 +5,6 @@ import { computed } from '@ember/object';
 export default Controller.extend({
   router: inject(),
   headerAndNavShown: computed('router.currentURL', function() {
-    console.log(
-      this.router.currentURL,
-      this.router.currentURL.includes('task'),
-    );
     return (
       this.router.currentURL.includes('task') ||
       this.router.currentURL.includes('loading')
