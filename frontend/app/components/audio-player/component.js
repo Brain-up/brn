b6696b130a36424c46557fc4d4649fbff89293be
Aleksandr Kanunnikov <lifeart92@gmail.com>
2020-04-05 18:24:10 +0300

    EPMLABSBRN-304 stop audio on destroy

--- frontend/app/components/audio-player/component.js
+++ frontend/app/components/audio-player/component.js
@@ -5,6 +5,11 @@ import { inject as service } from '@ember/service';
 export default class AudioPlayerComponent extends Component {
   @service audio;
   
+
+  willDestroy() {
+    this.audio.stop();
+  }
+
   get isPlaying() {
     return this.audio.isPlaying;
   }
