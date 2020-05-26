e1d2b79ab273828ee50b758644ec86a788c610fb
Aleksandr Kanunnikov <lifeart92@gmail.com>
2020-05-26 20:07:57 +0300

    style: application design updates

--- frontend/.template-lintrc.js
+++ frontend/.template-lintrc.js
@@ -1,10 +1,11 @@
 'use strict';
 
 module.exports = {
-  extends: 'octane',
+  plugins: ['ember-template-lint-plugin-tailwindcss'],
+  extends: ['octane', "ember-template-lint-plugin-tailwindcss:recommended"],
   rules: {
     'self-closing-void-elements': false,
-    'no-curly-component-invocation': { allow: ['group.name', 'not', 'html-safe', 'disablePlayButton', 'style-namespace'] },
+    'no-curly-component-invocation': { allow: ['group.name', 'not', 'html-safe', 'disablePlayButton', 'style-namespace', 'link-to'] },
     'no-implicit-this': { allow: ['styleNamespace', 'data-test-logo', 'data-test-series-navigation-list-link', 'data-test-group-link', 'data-test-series-navigation-list-link'] }
   }
 };
