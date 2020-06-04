e48727b911cc1c70d46157bb12aba58bc0396e95
Aleksandr Kanunnikov <lifeart92@gmail.com>
2020-06-05 01:21:16 +0300

    chore: fix tailwind colors

--- frontend/app/styles/tailwind.js
+++ frontend/app/styles/tailwind.js
@@ -1,9 +1,12 @@
 /* eslint-disable */
+const { colors } = require('tailwindcss/defaultTheme')
+
 module.exports = {
   theme: {
     extend: {
       colors: {
         blue: {
+          ...colors.blue,
           '500': '#81D5F9',
           '700': '#5E6EED',
           '900': '#001274',
