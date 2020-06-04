fccb1f062c37575784c5963229bb19c02676c226
Aleksandr Kanunnikov <lifeart92@gmail.com>
2020-06-04 11:00:28 +0300

    feature : locale switch

--- frontend/app/components/header/component.js
+++ frontend/app/components/header/component.js
@@ -5,10 +5,17 @@ import { action } from '@ember/object';
 export default class HeaderComponent extends Component {
   @service('session') session;
   @service('router') router;
+  @service('intl') intl;
 
   @action logout() {
     this.session.invalidate().then(() => {
       window.location.reload();
     });
+  }
+
+  @action setLocale(localeName) {
+    const name = localeName === 'ru' ? 'ru-ru': 'en-us';
+    this.intl.setLocale([name]);
+    this.router.transitionTo('groups.index', { queryParams: { reload: true } });
   }
 }
