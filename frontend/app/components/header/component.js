953c07be6235496ada5786661ce93564b8e382c4
Aleksandr Kanunnikov <lifeart92@gmail.com>
2020-06-05 00:42:21 +0300

    style: locale switch

--- frontend/app/components/header/component.js
+++ frontend/app/components/header/component.js
@@ -1,11 +1,14 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class HeaderComponent extends Component {
   @service('session') session;
   @service('router') router;
   @service('intl') intl;
+
+  @tracked selectedLocale = null;
 
   @action logout() {
     this.session.invalidate().then(() => {
@@ -13,9 +16,19 @@ export default class HeaderComponent extends Component {
     });
   }
 
+  get user() {
+    return this.session?.data?.user;
+  }
+
+  get activeLocale() {
+    return this.selectedLocale || this.intl.primaryLocale;
+  }
+
   @action setLocale(localeName) {
     const name = localeName === 'ru' ? 'ru-ru': 'en-us';
     this.intl.setLocale([name]);
+    this.selectedLocale = name;
+    localStorage.setItem('locale', name);
     this.router.transitionTo('groups.index', { queryParams: { reload: true } });
   }
 }
