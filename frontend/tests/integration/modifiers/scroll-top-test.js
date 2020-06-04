47acb448b8ee47d0d84353e6ac37e45065036524
Alex Kanunnikov <lifeart92@gmail.com>
2020-06-04 20:04:33 +0300

    style: group selection (#671)
    * style: group selection
    
    * fix lockfile
    
    * chore: fix linting issues
    
    * chore: fix audit

---
+++ frontend/tests/integration/modifiers/scroll-top-test.js
@@ -0,0 +1,15 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Modifier | scroll-top', function(hooks) {
+  setupRenderingTest(hooks);
+
+  // Replace this with your real tests.
+  test('it renders', async function(assert) {
+    await render(hbs`<div {{scroll-top}}></div>`);
+
+    assert.ok(true);
+  });
+});
