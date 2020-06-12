f750e5f5981ce88ccc1e2870b7db042559f6663b
Alex Kanunnikov <lifeart92@gmail.com>
2020-06-12 14:41:39 +0300

    EPMLABSBRN-341: fix duplicate email error (#704)

--- frontend/app/components/login-form/input/component.js
+++ frontend/app/components/login-form/input/component.js
@@ -17,7 +17,7 @@ export default class LoginFormInputComponent extends Component {
     const sumValue = (value || '').trim().length;
 
     if (sumValue >= this.maxlength - 1) {
-      return `${this.intl.t('rigistr_form.warning_input_restriction')} - ${
+      return `${this.intl.t('registration_form.warning_input_restriction')} - ${
         this.maxlength
       }`;
     }
