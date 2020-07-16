87e1148a0fea3ae8267f95c118d6ed0d18aadc5b
SiestaVie <vika.matvienk@gmail.com>
2020-07-16 11:33:08 +0300

    EPMLABSBRN-365 added max size for password

--- src/main/kotlin/com/epam/brn/dto/UserAccountDto.kt
+++ src/main/kotlin/com/epam/brn/dto/UserAccountDto.kt
@@ -26,7 +26,7 @@ data class UserAccountDto(
     )
     val email: String,
     @field:NotBlank(message = "{validation.field.password.blank}")
-    @field:Size(min = 4, max = 2147483647, message = "{validation.field.password.invalid-format}")
+    @field:Size(min = 4, max = 20, message = "{validation.field.password.invalid-format}")
     var password: String,
     val birthday: LocalDate? = null,
     var active: Boolean = true
