0d7b88ba2b1fecb0b02f20b57d4e70a19ead5c05
Aleksandr Kanunnikov <lifeart92@gmail.com>
2020-09-17 00:15:53 +0300

    add groups navigation

---
+++ frontend/app/controllers/group/series.js
@@ -0,0 +1,25 @@
+import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+export default class GroupSeriesController extends Controller {
+  queryParams = ['name']
+  @tracked name = ''
+  get exerciseName() {
+    return this.name;
+  }
+  get exerciseGroups() {
+    const items = {};
+    const exercises = this.model.exercises.toArray();
+    exercises.forEach((el)=>{
+      if (!(el.name in items)) {
+        items[el.name] = {
+          count: 0,
+          name: el.name,
+          picture: `/${el.pictureUrl}`
+        }
+      }
+      items[el.name].count++;
+    });
+    console.log( Object.values(items));
+    return Object.values(items);
+  }
+}
---
+++ frontend/tests/unit/controllers/group/series-test.js
@@ -0,0 +1,12 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+
+module('Unit | Controller | group/series', function(hooks) {
+  setupTest(hooks);
+
+  // TODO: Replace this with your real tests.
+  test('it exists', function(assert) {
+    let controller = this.owner.lookup('controller:group/series');
+    assert.ok(controller);
+  });
+});
