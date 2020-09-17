bd4337bbc770631210717630576a12db0b3f2fc6
Aleksandr Kanunnikov <lifeart92@gmail.com>
2020-09-17 09:53:46 +0300

    fix linting issue

--- frontend/tests/integration/components/timer/component-test.js
+++ frontend/tests/integration/components/timer/component-test.js
@@ -2,7 +2,6 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-import { timeout } from 'ember-concurrency';
 import mockService from '../../../test-support/mock-service';
 
 
