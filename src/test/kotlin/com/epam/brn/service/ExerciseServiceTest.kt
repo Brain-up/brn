5550f72071503a9b723f2e52248fb3a9458b3ce2
Elena Moshnikova <elena_moshnikova@epam.com>
2020-10-14 20:42:44 +0300

    epmlabsbrn-579

--- src/test/kotlin/com/epam/brn/service/ExerciseServiceTest.kt
+++ src/test/kotlin/com/epam/brn/service/ExerciseServiceTest.kt
@@ -45,7 +45,7 @@ internal class ExerciseServiceTest {
     fun `should get exercises by user`() {
         // GIVEN
         val exerciseMock: Exercise = mock(Exercise::class.java)
-        val exerciseDtoMock = ExerciseDto(2, 1, "name", "pictureUrl", "descr", 1, 0, NoiseDto(0, ""), ExerciseType.WORDS_SEQUENCES)
+        val exerciseDtoMock = ExerciseDto(2, 1, "name", "pictureUrl", "descr", 1, NoiseDto(0, ""), ExerciseType.WORDS_SEQUENCES)
         val exerciseId = 1L
         `when`(exerciseMock.toDto(true)).thenReturn(exerciseDtoMock)
         `when`(exerciseMock.id).thenReturn(exerciseId)
@@ -63,7 +63,7 @@ internal class ExerciseServiceTest {
     fun `should get exercises by user and series`() {
         // GIVEN
         val exerciseMock: Exercise = mock(Exercise::class.java)
-        val exerciseDtoMock = ExerciseDto(2, 1, "name", "pictureUrl", "descr", 1, 0, NoiseDto(0, ""), ExerciseType.WORDS_SEQUENCES)
+        val exerciseDtoMock = ExerciseDto(2, 1, "name", "pictureUrl", "descr", 1, NoiseDto(0, ""), ExerciseType.WORDS_SEQUENCES)
         val exerciseId = 1L
         val seriesId = 2L
         val userId = 3L
@@ -83,7 +83,7 @@ internal class ExerciseServiceTest {
     fun `should get exercise by id`() {
         // GIVEN
         val exerciseMock: Exercise = mock(Exercise::class.java)
-        val exerciseDtoMock = ExerciseDto(2, 1, "name", "pictureUrl", "descr", 1, 0, NoiseDto(0, ""), ExerciseType.WORDS_SEQUENCES)
+        val exerciseDtoMock = ExerciseDto(2, 1, "name", "pictureUrl", "descr", 1, NoiseDto(0, ""), ExerciseType.WORDS_SEQUENCES)
         `when`(exerciseMock.toDto()).thenReturn(exerciseDtoMock)
         `when`(exerciseRepository.findById(anyLong())).thenReturn(Optional.of(exerciseMock))
         // WHEN
