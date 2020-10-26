0e4adfcbe047c7dd765c66dcf6acd72ee31d5aec
Elena Moshnikova <elena_moshnikova@epam.com>
2020-10-26 12:39:09 +0300

    EPMLABSBRN-579 add calculation of availability

--- src/main/kotlin/com/epam/brn/repo/StudyHistoryRepository.kt
+++ src/main/kotlin/com/epam/brn/repo/StudyHistoryRepository.kt
@@ -1,5 +1,6 @@
 package com.epam.brn.repo
 
+import com.epam.brn.model.Exercise
 import com.epam.brn.model.StudyHistory
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
@@ -14,11 +15,17 @@ interface StudyHistoryRepository : CrudRepository<StudyHistory, Long> {
                 " WHERE s.exercise.series.id = :seriesId and s.userAccount.id = :userId"
     )
     fun getDoneExercisesIdList(@Param("seriesId") seriesId: Long, @Param("userId") userId: Long): List<Long>
+
+    @Query(
+        value = "SELECT s.exercise FROM StudyHistory s " +
+                " WHERE s.exercise.series.id = :seriesId and s.userAccount.id = :userId"
+    )
+    fun getDoneExercises(@Param("seriesId") seriesId: Long, @Param("userId") userId: Long): List<Exercise>
+
     @Query(
         value = "SELECT s.exercise.id FROM StudyHistory s " +
                 " WHERE s.userAccount.id = :userId"
     )
-
     fun getDoneExercisesIdList(@Param("userId") userId: Long): List<Long>
 
     fun findByUserAccountIdAndExerciseId(userId: Long, exerciseId: Long): Optional<StudyHistory>
