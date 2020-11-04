0bf914f438293d288ba9d88e523c7956d1dff2a3
Alex Kanunnikov <lifeart92@gmail.com>
2020-11-04 23:52:20 +0300

    global-timer-component (#1159)

---
+++ frontend/app/components/global-timer/index.ts
@@ -0,0 +1,37 @@
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import NetworkService from '../../services/network';
+import { task, timeout, Task as TaskGenerator } from 'ember-concurrency';
+import { tracked } from '@glimmer/tracking';
+import Ember from 'ember';
+
+export default class GlobalTimerComponent extends Component {
+  constructor(owner: any, args: any) {
+    super(owner, args);
+    this.syncTask.perform();
+  }
+  @service('network') declare network: NetworkService;
+  @tracked seconds = 0;
+  get minutes()  {
+    if (this.seconds <= 0) {
+      return 0;
+    }
+    return (this.seconds / 60).toFixed(1);
+  }
+  @(task(function*(this: GlobalTimerComponent) {
+    do {
+        try {
+          if (!Ember.testing) {
+            const response = yield this.network.request('study-history/todayTimer');
+            const { data } = yield response.json();
+            this.seconds = data;
+          }
+          yield timeout(10000);
+        } catch {
+          // ok
+        }
+    } while (true)
+
+  }).keepLatest())
+  syncTask!: TaskGenerator<any, any>;
+}
