2c81c71911af4f1c08453624943dfac4edc6ed16
Mikhail Moshnikov <62326146+Mikleprog@users.noreply.github.com>
2020-11-12 22:51:27 +0300

    EPMLABSBRN-637_Add_logo (#1171)
    * EPMLABSBRN-637_Add_logo
    
    * EPMLABSBRN-637_Add_logo_variant_4
    
    * EPMLABSBRN-637_Add_logo_variant_4_min
    
    * EPMLABSBRN-637_Add_logo_variant_4_
    
    * EPMLABSBRN-637_Add_logo_variant_4_test
    
    Co-authored-by: Mikhail Moshnikov <mikhail_moshnikov@epam.com>
    Co-authored-by: Alex Kanunnikov <lifeart92@gmail.com>

--- frontend/tests/integration/components/footer/component-test.js
+++ frontend/tests/integration/components/footer/component-test.js
@@ -9,7 +9,7 @@ module('Integration | Component | footer', function(hooks) {
   test('it renders', async function(assert) {
     await render(hbs`<Footer />`);
 
-    assert.dom('[data-test-support-logo]').exists({ count: 2 });
+    assert.dom('[data-test-support-logo]').exists({ count: 3 });
     assert.dom('[data-test-support-message]').exists();
   });
 });
