fc681fc9dd5d9fca4f9db06e9b25a825132d9167
Viktoryia_Khlystun <v-vkhlystun@expedia.com>
2020-12-17 01:16:50 +0300

    EPMLABSBRN-622: added processor for non speech exercises

--- src/main/kotlin/com/epam/brn/service/InitialDataLoader.kt
+++ src/main/kotlin/com/epam/brn/service/InitialDataLoader.kt
@@ -3,9 +3,6 @@ package com.epam.brn.service
 import com.epam.brn.auth.AuthorityService
 import com.epam.brn.model.Authority
 import com.epam.brn.model.UserAccount
-import com.epam.brn.model.ExerciseType
-import com.epam.brn.model.Exercise
-import com.epam.brn.model.Signal
 import com.epam.brn.model.Gender
 import com.epam.brn.repo.ExerciseGroupRepository
 import com.epam.brn.repo.ExerciseRepository
@@ -70,7 +67,8 @@ class InitialDataLoader(
         fileNameForSeries(1),
         fileNameForSeries(2),
         fileNameForSeries(3),
-        fileNameForSeries(4)
+        fileNameForSeries(4),
+        "non_speech.csv"
     )
 
     @EventListener(ApplicationReadyEvent::class)
@@ -138,63 +136,6 @@ class InitialDataLoader(
         else
             initDataFromClassPath()
         wordsService.fillWordsWithAudioOggFile()
-
-        create1SeriesNonSpeechGroup()
-    }
-
-    private fun create1SeriesNonSpeechGroup() {
-        val seriesLength = seriesRepository.findByNameLike("Длительность сигналов")[0]
-        val seriesFrequency = seriesRepository.findByNameLike("Частота сигналов")[0]
-        val exercise1 = Exercise(series = seriesLength, name = "По 2 сигнала разной длительности.", level = 1,
-            exerciseType = ExerciseType.TWO_DIFFERENT_LENGTH_SIGNAL.name)
-        val signalOne1 = Signal(frequency = 1000, length = 60, exercise = exercise1)
-        val signalTwo1 = Signal(frequency = 1000, length = 120, exercise = exercise1)
-        exercise1.addSignals(listOf(signalOne1, signalTwo1))
-        val exercise2 = Exercise(series = seriesLength, name = "По 2 сигнала разной длительности.", level = 2,
-            exerciseType = ExerciseType.TWO_DIFFERENT_LENGTH_SIGNAL.name)
-        val signalOne2 = Signal(frequency = 1000, length = 40, exercise = exercise2)
-        val signalTwo2 = Signal(frequency = 1000, length = 100, exercise = exercise2)
-        exercise2.addSignals(listOf(signalOne2, signalTwo2))
-
-        val exercise3 = Exercise(series = seriesFrequency, name = "По 2 сигнала разной частоты.", level = 1,
-            exerciseType = ExerciseType.THREE_DIFFERENT_FREQUENCY_SIGNAL.name)
-        val signalOne3 = Signal(frequency = 500, length = 120, exercise = exercise3)
-        val signalTwo3 = Signal(frequency = 1500, length = 120, exercise = exercise3)
-        exercise3.addSignals(listOf(signalOne3, signalTwo3))
-        val exercise4 = Exercise(series = seriesFrequency, name = "По 2 сигнала разной частоты.", level = 2,
-            exerciseType = ExerciseType.THREE_DIFFERENT_FREQUENCY_SIGNAL.name)
-        val signalOne4 = Signal(frequency = 1000, length = 120, exercise = exercise4)
-        val signalTwo4 = Signal(frequency = 2000, length = 120, exercise = exercise4)
-        exercise4.addSignals(listOf(signalOne4, signalTwo4))
-
-        val exercise5 = Exercise(series = seriesLength, name = "По 3 сигнала разной длительности.", level = 1,
-            exerciseType = ExerciseType.THREE_DIFFERENT_LENGTH_SIGNAL.name)
-        val signalOne5 = Signal(frequency = 1000, length = 40, exercise = exercise5)
-        val signalTwo5 = Signal(frequency = 1000, length = 90, exercise = exercise5)
-        val signalThree5 = Signal(frequency = 1000, length = 130, exercise = exercise5)
-        exercise5.addSignals(listOf(signalOne5, signalTwo5, signalThree5))
-        val exercise6 = Exercise(series = seriesLength, name = "По 3 сигнала разной длительности.", level = 2,
-            exerciseType = ExerciseType.THREE_DIFFERENT_LENGTH_SIGNAL.name)
-        val signalOne6 = Signal(frequency = 1000, length = 20, exercise = exercise6)
-        val signalTwo6 = Signal(frequency = 1000, length = 60, exercise = exercise6)
-        val signalThree6 = Signal(frequency = 1000, length = 120, exercise = exercise6)
-        exercise6.addSignals(listOf(signalOne6, signalTwo6, signalThree6))
-
-        val exercise7 = Exercise(series = seriesFrequency, name = "По 3 сигнала разной частоты.", level = 1,
-            exerciseType = ExerciseType.THREE_DIFFERENT_FREQUENCY_SIGNAL.name)
-        val signalOne7 = Signal(frequency = 500, length = 120, exercise = exercise7)
-        val signalTwo7 = Signal(frequency = 1050, length = 120, exercise = exercise7)
-        val signalThree7 = Signal(frequency = 2000, length = 120, exercise = exercise7)
-        exercise7.addSignals(listOf(signalOne7, signalTwo7, signalThree7))
-        val exercise8 = Exercise(series = seriesFrequency, name = "По 3 сигнала разной частоты.", level = 2,
-            exerciseType = ExerciseType.THREE_DIFFERENT_FREQUENCY_SIGNAL.name)
-        val signalOne8 = Signal(frequency = 200, length = 120, exercise = exercise8)
-        val signalTwo8 = Signal(frequency = 600, length = 120, exercise = exercise8)
-        val signalThree8 = Signal(frequency = 2000, length = 120, exercise = exercise8)
-        exercise8.addSignals(listOf(signalOne8, signalTwo8, signalThree8))
-
-        exerciseRepository.saveAll(listOf(exercise1, exercise2, exercise3, exercise4,
-            exercise4, exercise5, exercise6, exercise7, exercise8))
     }
 
     private fun initDataFromDirectory(directoryToScan: Path) {
