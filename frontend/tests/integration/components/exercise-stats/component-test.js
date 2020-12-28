9ecfd559d325849bcbeb5def4f3fdc964271dd33
Aleksandr Kanunnikov <lifeart92@gmail.com>
2020-12-28 22:35:24 +0300

    Revert "nojira-stats-modal"
    This reverts commit 8c033b893cc4485bfdbb2e87fdb05b90d7857b23.

--- frontend/tests/integration/components/exercise-stats/component-test.js
+++ frontend/tests/integration/components/exercise-stats/component-test.js
@@ -10,9 +10,7 @@ module('Integration | Component | exercise-stats', function(hooks) {
     this.set('stats', {
       endTime: new Date(1000),
       startTime: new Date(100),
-      rightAnswersCount: 10,
-      repeatsCount: 12,
-      wrongAnswersCount: 10
+      repetitionIndex: 1.66
     });
 
     this.set('onComplete', ()=> {
@@ -22,6 +20,7 @@ module('Integration | Component | exercise-stats', function(hooks) {
     await render(hbs`<ExerciseStats @stats={{this.stats}} @onComplete={{this.onComplete}} />`);
 
     assert.dom('[data-test-exercise-stats]').exists();
+    assert.dom('[data-test-type="positive"]').exists();
     assert.dom('[data-test-continue]').exists();
     await click('[data-test-continue]');
   });
