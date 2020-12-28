8c033b893cc4485bfdbb2e87fdb05b90d7857b23
Aleksandr Kanunnikov <lifeart92@gmail.com>
2020-12-28 22:34:41 +0300

    nojira-stats-modal

--- frontend/tests/integration/components/exercise-stats/component-test.js
+++ frontend/tests/integration/components/exercise-stats/component-test.js
@@ -10,7 +10,9 @@ module('Integration | Component | exercise-stats', function(hooks) {
     this.set('stats', {
       endTime: new Date(1000),
       startTime: new Date(100),
-      repetitionIndex: 1.66
+      rightAnswersCount: 10,
+      repeatsCount: 12,
+      wrongAnswersCount: 10
     });
 
     this.set('onComplete', ()=> {
@@ -20,7 +22,6 @@ module('Integration | Component | exercise-stats', function(hooks) {
     await render(hbs`<ExerciseStats @stats={{this.stats}} @onComplete={{this.onComplete}} />`);
 
     assert.dom('[data-test-exercise-stats]').exists();
-    assert.dom('[data-test-type="positive"]').exists();
     assert.dom('[data-test-continue]').exists();
     await click('[data-test-continue]');
   });
