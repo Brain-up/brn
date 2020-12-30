4c04febbcdb8c077240237084cc4af1ff4936fd9
Oleg <kalinon7@gmail.com>
2020-12-30 11:29:06 +0300

    [progress-sausage] implementing progress-sausage component (#1350)
    * [progress-sausage] implementing progress-sausage component
    
    * adds basic unit test for component
    
    * fix tests
    
    * fix unit test
    
    * feat add transition timings
    
    * purgecss styles fix
    
    Co-authored-by: Aleksandr Kanunnikov <lifeart92@gmail.com>

---
+++ frontend/tests/integration/components/progress-sausage/component-test.js
@@ -0,0 +1,35 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | progress-sausage', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function (assert) {
+
+    this.set('items', [
+      { completedInCurrentCycle: true },
+      { completedInCurrentCycle: false }
+    ]);
+
+    await render(hbs`<ProgressSausage @progressItems={{this.items}} />`);
+
+    assert.dom('[data-test-progress-sausage]').exists();
+    assert.dom('[data-test-progress-sausage]').hasAttribute('style', 'width:50%;');
+
+    this.set('items', [
+      { completedInCurrentCycle: false },
+      { completedInCurrentCycle: false }
+    ])
+
+    assert.dom('[data-test-progress-sausage]').hasAttribute('style', 'width:0%;');
+
+    this.set('items', [
+      { completedInCurrentCycle: true },
+      { completedInCurrentCycle: true }
+    ])
+
+    assert.dom('[data-test-progress-sausage]').hasAttribute('style', 'width:100%;');
+  });
+});
