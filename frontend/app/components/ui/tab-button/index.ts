cd85ad3eb2a4d467ec696afe865ee3e4088d1e6b
Aleksey Filatov <aleksei_filatov@epam.com>
2021-01-15 11:48:13 +0300

    styles changed

--- frontend/app/components/ui/tab-button/index.ts
+++ frontend/app/components/ui/tab-button/index.ts
@@ -2,17 +2,10 @@ import Component from '@glimmer/component';
 
 interface UiTabButtonComponentArguments {
     title?: string,
-    disabled?: boolean   
 }
 
 export default class UiTabButtonComponent extends Component<UiTabButtonComponentArguments> {
   get classes() {
-    const optionsForAll = 'focus:outline-none rounded w-1/6 uppercase h-12';
-
-    if (this.args.disabled)  {
-      return `${optionsForAll} button-disabled`
-    }
-      return `${optionsForAll} border-gradient button-options
-      hover:font-bold`
+    return 'focus:outline-none rounded w-1/6 uppercase h-12';      
   }
 }
