e0f692f61270a7e7b983172db1a9a136ebf5c9ea
Elena Moshnikova <elena_moshnikova@epam.com>
2021-02-05 10:47:14 +0300

    EPMLABBRN 764 try to delete DATA modifier, falls with exception

--- src/main/kotlin/com/epam/brn/model/Task.kt
+++ src/main/kotlin/com/epam/brn/model/Task.kt
@@ -79,4 +79,30 @@ data class Task(
         serialNumber = serialNumber,
         answerOptions = answerOptions.map { answer -> answer.toDto() }.toHashSet()
     )
+
+    override fun toString() = "Task(id=$id, name=$name, serialNumber=$serialNumber)"
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Task
+
+        if (id != other.id) return false
+        if (name != other.name) return false
+        if (serialNumber != other.serialNumber) return false
+        if (exercise != other.exercise) return false
+        if (correctAnswer != other.correctAnswer) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id?.hashCode() ?: 0
+        result = 31 * result + (name?.hashCode() ?: 0)
+        result = 31 * result + (serialNumber ?: 0)
+        result = 31 * result + (exercise?.hashCode() ?: 0)
+        result = 31 * result + (correctAnswer?.hashCode() ?: 0)
+        return result
+    }
 }
