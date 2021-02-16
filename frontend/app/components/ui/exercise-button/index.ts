f3a9e13cb8b2d87fd4418b51e7523664427a120f
AlexFil1983 <afilatov2002@gmail.com>
2021-02-16 13:46:11 +0300

    Exercise buttons (#1447)
    * check buttons changed
    
    exercise button added
    
    styles changed
    
    test template deleted
    
    inline styles deleted
    
    margin added to button
    
    integration part_1
    
    styles fixed
    
    styles and font size fixed
    
    font-size changed back to 100px
    
    * new buttons
    
    * fix tests
    
    * fix level usage
    
    Co-authored-by: Aleksei Filatov <aleksei_filatov@epam.com>
    Co-authored-by: Aleksandr Kanunnikov <lifeart92@gmail.com>

---
+++ frontend/app/components/ui/exercise-button/index.ts
@@ -0,0 +1,60 @@
+import Component from '@glimmer/component';
+import Exercise from 'brn/models/exercise';
+
+interface UiExerciseButtonComponentArguments {
+  title?: string;
+  exercise: Exercise;
+  isAvailable: boolean;
+}
+
+export default class UiExerciseButtonComponent extends Component<UiExerciseButtonComponentArguments> {
+  get classes() {
+    let items = ['focus:outline-none m-10'];
+    if (this.mode) {
+      items.push(this.mode);
+    }
+    return items.join(' ');
+  }
+  get mode() {
+    if (this.isDisabled) {
+      return 'disabled';
+    }
+    if (this.isCompleted) {
+      return 'completed';
+    }
+    if (this.isLocked) {
+      return 'locked';
+    }
+    return 'active';
+  }
+  get isDisabled() {
+    if (this.args.isAvailable === false) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  get isCompleted() {
+    return this.args.exercise.isCompleted;
+  }
+
+  get isActive() {
+    return this.mode === 'active';
+  }
+
+  get isLocked() {
+    return !this.args.isAvailable;
+  }
+
+  get titleClasses() {
+    if (this.mode === 'locked') {
+      return 'title title-locked';
+    }
+
+    if (this.mode === 'disabled') {
+      return 'title title-disabled';
+    }
+    return 'title';
+  }
+}
