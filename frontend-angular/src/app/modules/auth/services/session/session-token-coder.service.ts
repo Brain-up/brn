4d0c7bfac9280d7976a735da4b0f083fb796504e
alextanlan <alextanlan@gmail.com>
2021-02-26 21:09:51 +0300

    EPMLABSBRN-683 Implement encoding/decoding

--- frontend-angular/src/app/modules/auth/services/session/session-token-coder.service.ts
+++ frontend-angular/src/app/modules/auth/services/session/session-token-coder.service.ts
@@ -8,34 +8,32 @@ export class SessionTokenCoderService {
   static encodeToken(token: TokenOriginal): TokenShort {
     let {access_token, expires_in, token_start_date} = token;
     access_token = SessionTokenCoderService.encodeString(access_token);
-    expires_in = SessionTokenCoderService.encodeNumber(expires_in);
-    token_start_date = SessionTokenCoderService.encodeNumber(token_start_date);
-
     return SessionTokenMapperService.mapToShortToken({access_token, expires_in, token_start_date});
   }
 
   static decodeToken(token: TokenShort): TokenOriginal {
     let {t, e, s} = token;
     t = SessionTokenCoderService.decodeString(t);
-    e = SessionTokenCoderService.decodeNumber(e);
-    s = SessionTokenCoderService.decodeNumber(s);
-
     return SessionTokenMapperService.mapToOriginalToken({t, e, s});
   }
 
   static encodeString(input: string): string {
-    return '';
+    const encoded = btoa(input);
+    const reversed = SessionTokenCoderService.reverseString(encoded);
+    return SessionTokenCoderService.mixLetters(reversed);
   }
 
-  static decodeString(input: string): string {
-    return '';
+  static decodeString(encoded: string): string {
+    const unmixed = SessionTokenCoderService.mixLetters(encoded);
+    const reversed = SessionTokenCoderService.reverseString(unmixed);
+    return atob(reversed);
   }
 
-  static encodeNumber(input: number): number {
-    return 0;
+  private static reverseString(word: string): string {
+    return word.split('').reverse().join('');
   }
 
-  static decodeNumber(input: number): number {
-    return 0;
+  private static mixLetters(word: string): string {
+    return word.slice(-1) + word.slice(1, -1) + word.slice(0, 1);
   }
 }
