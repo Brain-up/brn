8f4ed2b12eb1725654a3ec0ed82943545034049a
Aleksandr Kanunnikov <lifeart92@gmail.com>
2021-03-05 12:55:05 +0300

    ex perms

--- frontend/app/routes/group/series/subgroup/exercise.ts
+++ frontend/app/routes/group/series/subgroup/exercise.ts
@@ -3,20 +3,30 @@ import { inject as service } from '@ember/service';
 import Exercise from 'brn/models/exercise';
 import type Transition from '@ember/routing/-private/transition';
 import TasksManagerService from 'brn/services/tasks-manager';
+import NetworkService from 'brn/services/network';
 
 export default class GroupSeriesSubgroupExerciseRoute extends Route {
   @service('tasks-manager')
   tasksManager!: TasksManagerService;
+  @service('network')
+  network!: NetworkService;
+
+  isAvailable: boolean = false;
 
   model({ exercise_id }: { exercise_id: string }) {
     return this.store.findRecord('exercise', exercise_id);
   }
 
   async afterModel(exercise: Exercise) {
+    const testable = await this.network.availableExercises([exercise.id]);
+    this.isAvailable = testable.includes(exercise.id);
     await exercise.hasMany('tasks').load();
   }
 
   redirect(exercise: Exercise, { to }: Transition) {
+    if (!this.isAvailable) {
+      return this.transitionTo('group.series.subgroup', exercise.get('parent.id'));
+    }
     if (exercise.hasMany('tasks').ids().length === 0) {
       alert(`Unable to find tasks for exercise ${exercise.get('id')}`);
       this.transitionTo('group.series', exercise.get('series.id'));
@@ -45,5 +55,6 @@ export default class GroupSeriesSubgroupExerciseRoute extends Route {
   }
   deactivate() {
     this.tasksManager.clearCurrentCycleTaks();
+    this.isAvailable = false;
   }
 }
