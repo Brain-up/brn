8d83c04e97ab0acb92970dbb31610ad081da03d9
Elena <elena_moshnikova@epam.com>
2021-03-16 11:04:59 +0300

    EPMLABSBRN-819 update versions, fix n+1 problem (#1688)
    * EPMLABSBRN-819 upgrade versions, delete unused, fix n+1 problem

--- src/main/kotlin/com/epam/brn/model/AudiometryHistory.kt
+++ src/main/kotlin/com/epam/brn/model/AudiometryHistory.kt
@@ -21,11 +21,11 @@ class AudiometryHistory(
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     var userAccount: UserAccount,
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "audiometry_task_id")
     var audiometryTask: AudiometryTask,
     @Column(nullable = false)
