cb0ed7eb7d2f40605272a32cf1557d70916ec074
Elena Moshnikova <elena_moshnikova@epam.com>
2021-03-24 11:24:09 +0300

    EPMLABSBRN-824 redesign /admin/users end-point

--- src/main/kotlin/com/epam/brn/service/UserAccountService.kt
+++ src/main/kotlin/com/epam/brn/service/UserAccountService.kt
@@ -3,7 +3,9 @@ package com.epam.brn.service
 import com.epam.brn.dto.request.UserAccountChangeRequest
 import com.epam.brn.dto.request.UserAccountCreateRequest
 import com.epam.brn.dto.response.UserAccountDto
+import com.epam.brn.dto.response.UserWithAnalyticsDto
 import com.epam.brn.model.UserAccount
+import org.springframework.data.domain.Pageable
 
 interface UserAccountService {
     fun findUserByName(name: String): UserAccountDto
@@ -12,7 +14,8 @@ interface UserAccountService {
     fun save(userAccountCreateRequest: UserAccountCreateRequest): UserAccountDto
     fun findUserById(id: Long): UserAccountDto
     fun getUserFromTheCurrentSession(): UserAccountDto
-    fun getUsers(): List<UserAccountDto>
+    fun getUsers(pageable: Pageable): List<UserAccountDto>
+    fun getUsersWithAnalytics(pageable: Pageable): List<UserWithAnalyticsDto>
     fun updateAvatarForCurrentUser(avatarUrl: String): UserAccountDto
     fun updateCurrentUser(userChangeRequest: UserAccountChangeRequest): UserAccountDto
 
