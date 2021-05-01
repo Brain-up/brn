e614fa35f4c06f8cd0968a7419303e492f266d1c
Dmitrii Komlev <dmitrii_komlev@epam.com>
2021-05-01 15:51:39 +0300

    added logic and content for statistics-info-dialog component

--- frontend-angular/src/app/modules/admin/modules/statistics/statistics.component.ts
+++ frontend-angular/src/app/modules/admin/modules/statistics/statistics.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { StatisticsInfoDialogComponent } from './components/statistics-info-dialog/statistics-info-dialog.component';
 
 @Component({
   selector: 'app-statistics',
@@ -6,4 +8,18 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./statistics.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StatisticsComponent {}
+export class StatisticsComponent implements OnDestroy {
+  private statisticsInfoDialogRef: MatDialogRef<StatisticsInfoDialogComponent, void>;
+
+  constructor(public readonly matDialog: MatDialog) {}
+
+  ngOnDestroy(): void {
+    this.statisticsInfoDialogRef?.close();
+  }
+
+  public openStatisticsInfoDialog(): void {
+    this.statisticsInfoDialogRef = this.matDialog.open(StatisticsInfoDialogComponent, {
+      width: '650px',
+    });
+  }
+}
--- frontend-angular/src/app/modules/admin/modules/statistics/statistics.module.ts
+++ frontend-angular/src/app/modules/admin/modules/statistics/statistics.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
 import { StatisticsInfoDialogComponent } from './components/statistics-info-dialog/statistics-info-dialog.component';
 import { SumTimeTrackComponent } from './components/sum-time-track/sum-time-track.component';
 import { UnitTimeTrackComponent } from './components/unit-time-track/unit-time-track.component';
 import { StatisticsRoutingModule } from './statistics-routing.module';
 import { StatisticsComponent } from './statistics.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    StatisticsComponent,
-    SumTimeTrackComponent,
-    UnitTimeTrackComponent,
-    StatisticsInfoDialogComponent,
-  ],
-  imports: [StatisticsRoutingModule],
+  declarations: [StatisticsComponent, StatisticsInfoDialogComponent, UnitTimeTrackComponent, SumTimeTrackComponent],
+  imports: [StatisticsRoutingModule, MatButtonModule, MatDialogModule, MatIconModule],
 })
 export class StatisticsModule {}
