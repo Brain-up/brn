3794bd214ea39c3aa76b07c694706f56c86f17d5
Dmitrii Komlev <dmitrii_komlev@epam.com>
2021-05-01 19:16:34 +0300

    added unit tests for statistics-info-dialog component

--- frontend-angular/src/app/modules/admin/modules/statistics/components/statistics-info-dialog/statistics-info-dialog.component.spec.ts
+++ frontend-angular/src/app/modules/admin/modules/statistics/components/statistics-info-dialog/statistics-info-dialog.component.spec.ts
@@ -1,21 +1,42 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { StatisticsInfoDialogComponent } from './statistics-info-dialog.component';
 
 describe('StatisticsInfoDialogComponent', () => {
-  let component: StatisticsInfoDialogComponent;
   let fixture: ComponentFixture<StatisticsInfoDialogComponent>;
-
-  beforeEach(
-    waitForAsync(() => {
-      TestBed.configureTestingModule({
-        declarations: [StatisticsInfoDialogComponent],
-      }).compileComponents();
-    })
-  );
+  let component: StatisticsInfoDialogComponent;
+  let hostElement: HTMLElement;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [StatisticsInfoDialogComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+    });
+
     fixture = TestBed.createComponent(StatisticsInfoDialogComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    hostElement = fixture.nativeElement;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should image source of dialog content section is valid', async () => {
+    const imgElem = hostElement.querySelector<HTMLImageElement>('img.statistics-info-dialog');
+
+    const fakeImgElem = new Image();
+    const loadingImg = new Promise<{ type: string }>((resolve, reject) => {
+      fakeImgElem.onload = resolve;
+      fakeImgElem.onerror = reject;
+    });
+    fakeImgElem.src = imgElem.src;
+
+    try {
+      const result = await loadingImg;
+      expect(result.type).toBe('load');
+    } catch (result) {
+      expect(result.type).not.toBe('error');
+    }
   });
 });
