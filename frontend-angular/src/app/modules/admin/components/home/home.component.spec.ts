0a4f3af1b3f97051628cf8b5a4012b6f37638f71
Dmitrii Komlev <dmitrii_komlev@epam.com>
2021-05-07 14:40:08 +0300

    fixed unit test for home component from admin module

--- frontend-angular/src/app/modules/admin/components/home/home.component.spec.ts
+++ frontend-angular/src/app/modules/admin/components/home/home.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('HomeComponent', () => {
   let fixture: ComponentFixture<HomeComponent>;
@@ -8,6 +9,7 @@ describe('HomeComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [HomeComponent],
+      imports: [RouterTestingModule],
     });
 
     fixture = TestBed.createComponent(HomeComponent);
