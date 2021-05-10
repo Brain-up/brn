023392cc49a22a781d3bf2672334d4a87f65b5c0
Richárd Forgó <richard_forgo@epam.com>
2021-05-10 14:56:56 +0200

    [EPMLABSBRN-841] fix: ktlint unused import

--- src/test/kotlin/com/epam/brn/controller/UserDetailControllerTest.kt
+++ src/test/kotlin/com/epam/brn/controller/UserDetailControllerTest.kt
@@ -15,7 +15,6 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
-import org.mockito.Mockito
 import org.mockito.Mockito.`when`
 import org.mockito.junit.jupiter.MockitoExtension
 import kotlin.test.assertEquals
