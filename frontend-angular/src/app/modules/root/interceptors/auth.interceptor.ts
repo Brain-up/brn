177bb9953a3de2de8f99d88d3fbf75bd660ee104
Dmitrii Komlev <dmitrii_komlev@epam.com>
2021-05-12 15:41:31 +0300

    added translations system and translated some part of codebase

--- frontend-angular/src/app/modules/root/interceptors/auth.interceptor.ts
+++ frontend-angular/src/app/modules/root/interceptors/auth.interceptor.ts
@@ -5,20 +5,15 @@ import { AuthTokenService } from '@root/services/auth-token.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  private readonly URL_BLACK_LIST = new Set([
-    'http://testbucket12356123456.s3.amazonaws.com',
-    'https://s3.us-south.cloud-object-storage.appdomain.cloud/cloud-object-storage-gg-cos-standard-koy',
-  ]);
-
   constructor(private readonly authTokenService: AuthTokenService) {}
 
   public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const authToken = this.authTokenService.getAuthToken();
 
-    if (!this.URL_BLACK_LIST.has(req.url) && authToken) {
-      req.headers.set('Authorization', `Bearer ${authToken.access_token}`);
+    if (authToken) {
+      return next.handle(req.clone({ setHeaders: { Authorization: `Bearer ${authToken.access_token}` } }));
     }
 
-    return next.handle(req.clone({ headers: req.headers }));
+    return next.handle(req);
   }
 }
--- frontend-angular/src/app/modules/shared/constants/common-constants.ts
+++ frontend-angular/src/app/modules/shared/constants/common-constants.ts
@@ -1,2 +1,4 @@
 export const AUTH_PAGE = '/auth';
 export const HOME_PAGE = '/upload-file';
+
+export const DEFAULT_LANG = 'en';
