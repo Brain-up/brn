1592f024811b09fcc657c76191897e8fa567e7c4
andrsam <andrsbox@gmail.com>
2021-05-20 18:32:32 +0300

    EPMLABSBRN-823 fix star import

--- src/main/kotlin/com/epam/brn/config/JpaAuditConfig.kt
+++ src/main/kotlin/com/epam/brn/config/JpaAuditConfig.kt
@@ -8,7 +8,7 @@ import org.springframework.data.auditing.DateTimeProvider
 import org.springframework.data.domain.AuditorAware
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.security.core.context.SecurityContextHolder
-import java.util.*
+import java.util.Optional
 
 @Configuration
 @EnableJpaAuditing(
