e3f286fb4fcc76a2559b395d5a521366973091b7
Roman Konoplev <rkonoplev@gmail.com>
2021-05-21 12:27:06 +0100

    Unit test for AudioController has changed

--- src/test/kotlin/com/epam/brn/controller/AudioControllerTest.kt
+++ src/test/kotlin/com/epam/brn/controller/AudioControllerTest.kt
@@ -16,7 +16,6 @@ import java.io.InputStream
 import java.io.ByteArrayInputStream
 import kotlin.test.assertEquals
 
-@Suppress("EqualsBetweenInconvertibleTypes")
 @ExtendWith(MockKExtension::class)
 
 internal class AudioControllerTest {
