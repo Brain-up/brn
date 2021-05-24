28c2ea0a1f6c3c7b8ecb46fdd3de00f3431b1839
Dmitrii Komlev <dmitrii_komlev@epam.com>
2021-05-24 16:16:08 +0300

    fixed tests

--- frontend-angular/src/app/modules/root/components/not-found/not-found.component.spec.ts
+++ frontend-angular/src/app/modules/root/components/not-found/not-found.component.spec.ts
@@ -1,13 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateModule } from '@ngx-translate/core';
 import { NotFoundComponent } from './not-found.component';
 
 describe('NotFoundComponent', () => {
-  let fixture: ComponentFixture<AdminComponent>;
+  let fixture: ComponentFixture<NotFoundComponent>;
   let component: NotFoundComponent;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [NotFoundComponent],
+      imports: [TranslateModule.forRoot()],
     });
 
     fixture = TestBed.createComponent(NotFoundComponent);
