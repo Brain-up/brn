5134efc71ea5df4b458ae883e73e55db6d3ff0d4
Dmitrii_Fokin <fokinds1@gmail.com>
2021-06-07 14:23:20 +0300

    EPMLABSBRN-693 change mackito to mockk in HeadphonesServiceImplTest

--- src/test/kotlin/com/epam/brn/service/impl/HeadphonesServiceImplTest.kt
+++ src/test/kotlin/com/epam/brn/service/impl/HeadphonesServiceImplTest.kt
@@ -7,43 +7,50 @@ import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import com.natpryce.hamkrest.hasSize
 import com.natpryce.hamkrest.sameInstance
-import org.junit.jupiter.api.DisplayName
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.InjectMocks
-import org.mockito.Mock
-import org.mockito.Mockito.`when`
-import org.mockito.junit.jupiter.MockitoExtension
 
-@ExtendWith(MockitoExtension::class)
-@DisplayName("Headphone service test using mockito")
+@ExtendWith(MockKExtension::class)
 internal class HeadphonesServiceImplTest {
-    @InjectMocks
+
+    @InjectMockKs
     lateinit var headphonesServiceImpl: HeadphonesServiceImpl
 
-    @Mock
+    @MockK
     lateinit var headphonesRepository: HeadphonesRepository
-    private val headphonesName = "test"
-    private val headphonesEntity = Headphones(name = headphonesName, type = HeadphonesType.IN_EAR_BLUETOOTH)
-    private val headphonesEntitySecond = Headphones(name = headphonesName, type = HeadphonesType.ON_EAR_NO_BLUETOOTH)
+
+    companion object {
+        private const val headphonesName = "test"
+        private val headphonesEntity = Headphones(name = headphonesName, type = HeadphonesType.IN_EAR_BLUETOOTH)
+        private val headphonesEntitySecond =
+            Headphones(name = headphonesName, type = HeadphonesType.ON_EAR_NO_BLUETOOTH)
+    }
 
     @Test
     fun `should save to the repository and return dto`() {
-        `when`(headphonesRepository.save(headphonesEntity)).thenReturn(headphonesEntity)
+        // GIVEN
+        every { headphonesRepository.save(headphonesEntity) } returns headphonesEntity
+
+        // WHEN
         val headphonesDto = headphonesServiceImpl.save(headphonesEntity)
+
+        // THEN
         assertThat(headphonesDto.name, sameInstance(headphonesName))
     }
 
     @Test
-    fun getAllHeadphonesForUser() {
-        `when`(headphonesRepository.getHeadphonesForUser(1L)).thenReturn(
-            listOf(
-                headphonesEntity,
-                headphonesEntitySecond
-            )
-        )
+    fun `should get all headphones for user`() {
+        // GIVEN
+        every { headphonesRepository.getHeadphonesForUser(1L) } returns listOf(headphonesEntity, headphonesEntitySecond)
 
+        // WHEN
         val allHeadphonesForUser = headphonesServiceImpl.getAllHeadphonesForUser(1L)
+
+        // THEN
         assertThat(allHeadphonesForUser, hasSize(equalTo(2)))
     }
 }
