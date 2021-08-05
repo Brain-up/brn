72ca0d5e64b98595cc16b1353ae5312f4a17cae6
Aleksandr Kanunnikov <lifeart92@gmail.com>
2021-08-05 12:54:33 +0300

    frequency-words support

---
+++ frontend/app/models/task/frequency-words.ts
@@ -0,0 +1,28 @@
+import { attr } from '@ember-data/model';
+import BaseTask from '../task';
+import shuffleArray from 'brn/utils/shuffle-array';
+import { cached } from 'tracked-toolbox';
+
+export default class TaskFrequencyWordsModel extends BaseTask {
+  @attr() answerOptions!: any;
+  exerciseType = 'single-simple-words';
+  @cached
+  get tasksToSolve() {
+    return [
+      ...shuffleArray(this.answerOptions),
+      ...shuffleArray(this.answerOptions),
+    ].map((item, index) => {
+      return {
+        answer: [item],
+        order: index,
+      };
+    });
+  }
+}
+
+// DO NOT DELETE: this is how TypeScript knows how to look up your models.
+declare module 'ember-data/types/registries/model' {
+  export default interface ModelRegistry {
+    'task/frequency-words': TaskFrequencyWordsModel;
+  }
+}
