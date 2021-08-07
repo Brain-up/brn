cb8360ff13a9a7f3e8e9ddb7c28c86ceab98fbe6
Veronica <fikase.2198@gmail.com>
2021-08-07 12:34:14 +0300

    ISSUE-2016:[BE] Unit tests: StudyHistoryService.kt, ResourceService.kt, TaskService.kt
    Test for ResourceService.kt

--- src/test/kotlin/com/epam/brn/service/ResourceServiceTest.kt
+++ src/test/kotlin/com/epam/brn/service/ResourceServiceTest.kt
@@ -4,15 +4,17 @@ import com.epam.brn.dto.ResourceDto
 import com.epam.brn.exception.EntityNotFoundException
 import com.epam.brn.model.Resource
 import com.epam.brn.repo.ResourceRepository
+import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
 import io.mockk.verify
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.amshove.kluent.shouldBeNull
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.data.repository.findByIdOrNull
 import java.util.Optional
 
 @ExtendWith(MockKExtension::class)
@@ -24,8 +26,77 @@ internal class ResourceServiceTest {
     @MockK
     lateinit var resourceRepository: ResourceRepository
 
+    @MockK
+    lateinit var resource: Resource
+
     val id = 1L
     val description = "I'd like to add a description"
+
+    @Test
+    fun `should return first resource by word if not empty`() {
+        // GIVEN
+        val word = "word"
+        every { resourceRepository.findByWordLike(word) } returns listOf(resource)
+
+        // WHEN
+        val foundFirstResource = resourceService.findFirstResourceByWordLike(word)
+
+        // THEN
+        foundFirstResource.shouldBe(resource)
+    }
+
+    @Test
+    fun `should return null if the word is not found`() {
+        // GIVEN
+        val word = "word"
+        every { resourceRepository.findByWordLike(word) } returns emptyList()
+
+        // WHEN
+        val foundFirstResource = resourceService.findFirstResourceByWordLike(word)
+
+        // THEN
+        foundFirstResource.shouldBeNull()
+    }
+
+    @Test
+    fun `should return resource by word and audio file url`() {
+        // GIVEN
+        val word = "word"
+        val audioFileName = "audioFileName"
+        every { resourceRepository.findFirstByWordAndAudioFileUrlLike(word, audioFileName) } returns Optional.of(resource)
+
+        // WHEN
+        val foundResource = resourceService.findFirstByWordAndAudioFileUrlLike(word, audioFileName)
+
+        // THEN
+        foundResource.shouldBe(resource)
+    }
+
+    @Test
+    fun `should return null if word and audio file url is not found`() {
+        // GIVEN
+        val word = "word"
+        val audioFileName = "audioFileName"
+        every { resourceRepository.findFirstByWordAndAudioFileUrlLike(word, audioFileName) } returns Optional.empty()
+
+        // WHEN
+        val foundResource = resourceService.findFirstByWordAndAudioFileUrlLike(word, audioFileName)
+
+        // THEN
+        foundResource.shouldBeNull()
+    }
+
+    @Test
+    fun `should save resource`() {
+        // GIVEN
+        every { resourceRepository.save(resource) } returns resource
+
+        // WHEN
+        val result = resourceService.save(resource)
+
+        // THEN
+        result.shouldBe(resource)
+    }
 
     @Test
     fun `should update description successfully`() {
@@ -34,24 +105,25 @@ internal class ResourceServiceTest {
             id = id,
             wordType = "OBJECT"
         )
-        every { resourceRepository.findById(id) } returns Optional.of(resource)
+        every { resourceRepository.findByIdOrNull(id) } returns resource
         every { resourceRepository.save(resource) } returns resource
 
         // WHEN
         val result: ResourceDto = resourceService.updateDescription(id, description)
 
         // THEN
-        verify(exactly = 1) { resourceRepository.findById(id) }
+        verify(exactly = 1) { resourceRepository.findByIdOrNull(id) }
         verify(exactly = 1) { resourceRepository.save(resource) }
 
-        assertEquals(id, result.id)
-        assertEquals(description, result.description)
+        id.shouldBe(result.id)
+        description.shouldBe(result.description)
     }
 
     @Test
     fun `Should throw EntityNotFoundException if it does not exist`() {
         // GIVEN
-        every { resourceRepository.findById(id) } returns Optional.empty()
+        val expectedErrorMessage = "Resource not found by id=$id"
+        every { resourceRepository.findByIdOrNull(id) } returns null
 
         // WHEN
         val exception = assertThrows<EntityNotFoundException> {
@@ -59,7 +131,7 @@ internal class ResourceServiceTest {
         }
 
         // THEN
-        assertEquals("Resource not found by id=$id", exception.message)
-        verify(exactly = 1) { resourceRepository.findById(id) }
+        expectedErrorMessage.shouldBe(exception.message)
+        verify(exactly = 1) { resourceRepository.findByIdOrNull(id) }
     }
 }
