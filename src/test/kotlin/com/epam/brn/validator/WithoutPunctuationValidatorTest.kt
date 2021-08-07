844aa1c53b66a1a2bc8bc3e53a3d51f770e40b6e
Dmitrii_Fokin <fokinds1@gmail.com>
2021-08-07 12:39:10 +0300

    EPMLABSBRN-885 added tests for WithoutPunctuationValidator

---
+++ src/test/kotlin/com/epam/brn/validator/WithoutPunctuationValidatorTest.kt
@@ -0,0 +1,44 @@
+package com.epam.brn.validator
+
+import io.kotest.matchers.booleans.shouldBeFalse
+import io.kotest.matchers.booleans.shouldBeTrue
+import org.junit.jupiter.api.Test
+
+internal class WithoutPunctuationValidatorTest {
+
+    private val withoutPunctuationValidator = WithoutPunctuationValidator()
+
+    @Test
+    fun `should be validate string`() {
+        // GIVEN
+        val testStringEN = " same text"
+        val testStringRU = "тестовый текст"
+        val testStringWithNum = "same text with 123"
+
+        // WHEN
+        val resultEN = withoutPunctuationValidator.isValid(testStringEN, null)
+        val resultRU = withoutPunctuationValidator.isValid(testStringRU, null)
+        val resultNum = withoutPunctuationValidator.isValid(testStringWithNum, null)
+
+        resultEN.shouldBeTrue()
+        resultRU.shouldBeTrue()
+        resultNum.shouldBeTrue()
+    }
+
+    @Test
+    fun `should not validate string`() {
+        // GIVEN
+        val testStringEN = " same text!"
+        val testStringRU = " какой-то текст."
+        val testStringWithNum = "same, text with 123"
+
+        // WHEN
+        val resultEN = withoutPunctuationValidator.isValid(testStringEN, null)
+        val resultRU = withoutPunctuationValidator.isValid(testStringRU, null)
+        val resultNum = withoutPunctuationValidator.isValid(testStringWithNum, null)
+
+        resultEN.shouldBeFalse()
+        resultRU.shouldBeFalse()
+        resultNum.shouldBeFalse()
+    }
+}
