cbc0a0cc312a2f575d303c2536adba2c270d7c70
Veronica <fikase.2198@gmail.com>
2021-08-07 13:58:50 +0300

    ISSUE-2016:[BE] Unit tests: StudyHistoryService.kt, ResourceService.kt, TaskService.kt
    Test for StudyHistoryService.kt

--- src/test/kotlin/com/epam/brn/service/StudyHistoryServiceTest.kt
+++ src/test/kotlin/com/epam/brn/service/StudyHistoryServiceTest.kt
@@ -1,27 +1,29 @@
 package com.epam.brn.service
 
 import com.epam.brn.dto.StudyHistoryDto
+import com.epam.brn.dto.response.UserAccountDto
 import com.epam.brn.model.Exercise
 import com.epam.brn.model.Gender
 import com.epam.brn.model.StudyHistory
 import com.epam.brn.model.UserAccount
 import com.epam.brn.repo.ExerciseRepository
 import com.epam.brn.repo.StudyHistoryRepository
+import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.SpyK
 import io.mockk.junit5.MockKExtension
 import io.mockk.verify
-import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import java.time.LocalDateTime
 import java.util.Optional
-import kotlin.test.assertEquals
 
 @ExtendWith(MockKExtension::class)
 internal class StudyHistoryServiceTest {
 
+    @SpyK
     @InjectMockKs
     lateinit var studyHistoryService: StudyHistoryService
 
@@ -36,6 +38,33 @@ internal class StudyHistoryServiceTest {
 
     @MockK
     lateinit var studyHistoryDtoMock: StudyHistoryDto
+
+    @MockK
+    lateinit var currentUser: UserAccountDto
+
+    @MockK
+    lateinit var studyHistory: StudyHistory
+
+    @MockK
+    lateinit var from: LocalDateTime
+
+    @MockK
+    lateinit var to: LocalDateTime
+
+    @Test
+    fun `should return today timer`() {
+        // GIVEN
+        val timer = 1
+        every { userAccountService.getUserFromTheCurrentSession() } returns currentUser
+        every { currentUser.id } returns 1L
+        every { studyHistoryRepository.getTodayDayTimer(1L) } returns timer
+
+        // WHEN
+        val todayTimer = studyHistoryService.getTodayTimer()
+
+        // THEN
+        todayTimer.shouldBe(timer)
+    }
 
     @Test
     fun `should create studyHistory when doesn't exist`() {
@@ -83,18 +112,79 @@ internal class StudyHistoryServiceTest {
 
         // THEN
         verify(exactly = 1) { studyHistoryRepository.save(ofType(StudyHistory::class)) }
-        Assertions.assertThat(result).isEqualToIgnoringGivenFields(studyHistorySaved, "id", "exerciseId")
+        result.shouldBe(studyHistorySaved.toDto())
     }
 
     @Test
     fun `should calculate diff in seconds between start and end time`() {
         // GIVEN
+        val expectedTimer = 60
         val now = LocalDateTime.now()
 
         // WHEN
         val result = studyHistoryService.calculateDiffInSeconds(now, now.plusMinutes(1))
 
         // THEN
-        assertEquals(60, result)
+        result.shouldBe(expectedTimer)
+    }
+
+    @Test
+    fun `should return histories for current user`() {
+        // GIVEN
+        val expectedStudyHistoryDto = listOf(studyHistoryDtoMock)
+        every { userAccountService.getUserFromTheCurrentSession() } returns currentUser
+        every { currentUser.id } returns 1L
+        every { studyHistoryService.getHistories(1L, from, to) } returns expectedStudyHistoryDto
+
+        // WHEN
+        val historiesForCurrentUser = studyHistoryService.getHistoriesForCurrentUser(from, to)
+
+        // THEN
+        historiesForCurrentUser.shouldBe(expectedStudyHistoryDto)
+    }
+
+    @Test
+    fun `should return histories`() {
+        // GIVEN
+        val expectedStudyHistory = listOf(studyHistory)
+        every { studyHistory.toDto() } returns studyHistoryDtoMock
+        every { studyHistoryRepository.findAllByUserAccountIdAndStartTimeBetween(1L, from, to) } returns expectedStudyHistory
+
+        // WHEN
+        val histories = studyHistoryService.getHistories(1L, from, to)
+
+        // THEN
+        val expectedStudyHistoryDto = expectedStudyHistory.map { it.toDto() }
+        histories.shouldBe(expectedStudyHistoryDto)
+    }
+
+    @Test
+    fun `should return month histories for current user`() {
+        // GIVEN
+        val expectedStudyHistoryDto = listOf(studyHistoryDtoMock)
+        every { userAccountService.getUserFromTheCurrentSession() } returns currentUser
+        every { currentUser.id } returns 1L
+        every { studyHistoryService.getMonthHistories(1L, 1, 1) } returns expectedStudyHistoryDto
+
+        // WHEN
+        val monthHistoriesForCurrentUser = studyHistoryService.getMonthHistoriesForCurrentUser(1, 1)
+
+        // THEN
+        monthHistoriesForCurrentUser.shouldBe(expectedStudyHistoryDto)
+    }
+
+    @Test
+    fun `should return month histories`() {
+        // GIVEN
+        val expectedStudyHistory = listOf(studyHistory)
+        every { studyHistory.toDto() } returns studyHistoryDtoMock
+        every { studyHistoryRepository.getMonthHistories(1L, 1, 1) } returns expectedStudyHistory
+
+        // WHEN
+        val histories = studyHistoryService.getMonthHistories(1L, 1, 1)
+
+        // THEN
+        val expectedStudyHistoryDto = expectedStudyHistory.map { it.toDto() }
+        histories.shouldBe(expectedStudyHistoryDto)
     }
 }
