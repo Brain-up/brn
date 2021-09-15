80d73ebb6a28ef6e0bb713f09c45c0169a31dc4f
Oksana <ignatyuk.oksana@yandex.ru>
2021-09-15 23:52:59 +0600

    EPMLABSBRN-885 resolve merge conflict

--- src/main/kotlin/com/epam/brn/service/ExerciseService.kt
+++ src/main/kotlin/com/epam/brn/service/ExerciseService.kt
@@ -2,7 +2,6 @@ package com.epam.brn.service
 
 import com.epam.brn.dto.ExerciseDto
 import com.epam.brn.dto.ExerciseWithTasksResponse
-import com.epam.brn.dto.ExerciseWithTasksDto
 import com.epam.brn.dto.request.exercise.ExerciseCreateDto
 import com.epam.brn.dto.request.exercise.ExercisePhrasesCreateDto
 import com.epam.brn.dto.request.exercise.ExerciseSentencesCreateDto
