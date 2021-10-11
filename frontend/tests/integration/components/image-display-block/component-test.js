b33d117b715dfcb72a5b4952bc4eff390f065c1e
Mikhail_Moshnikov <mikhai_moshnikov@epam.com>
2021-10-11 11:14:09 +0300

    2094 test

--- frontend/tests/integration/components/image-display-block/component-test.js
+++ frontend/tests/integration/components/image-display-block/component-test.js
@@ -24,7 +24,7 @@ module('Integration | Component | image-display-block', function (hooks) {
     );
     assert.equal(
       pageObject.imageAttribute,
-      '--word-picture-url: url(/pictures/вить.jpg);',
+      '--word-picture-url:url(\\/pictures\\/вить\\.jpg);',
       'has a right image variable',
     );
   });
