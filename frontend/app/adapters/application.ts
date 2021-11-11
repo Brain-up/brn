0d5d18cc170dc8db18166ef52d044747f036841d
Koylubaev Nikita <koylubaevnt@gmail.com>
2021-11-11 13:48:50 +0500

    EPMLABSBRN-780: BE: implement Firebase authentification
    * Fix Authorization header

--- frontend/app/adapters/application.ts
+++ frontend/app/adapters/application.ts
@@ -10,7 +10,7 @@ export default class ApplicationAdapter extends RESTAdapter {
       return {};
     }
     return {
-      Authorization: `Basic ${this.session.data?.authenticated.user.stsTokenManager.accessToken}`,
+      Authorization: `Bearer ${this.session.data?.authenticated.user.stsTokenManager.accessToken}`,
     };
   }
   namespace = 'api';
