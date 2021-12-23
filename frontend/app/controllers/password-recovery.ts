32186d7ec0628ace9901e5d144476d45537a19c5
Aleksandr Kanunnikov <lifeart92@gmail.com>
2021-12-23 12:12:16 +0300

    use firebase logic to reset password

--- frontend/app/controllers/password-recovery.ts
+++ frontend/app/controllers/password-recovery.ts
@@ -3,14 +3,20 @@ import { action } from '@ember/object';
 import { getOwner } from '@ember/application';
 import type FirebaseAuthenticator from 'brn/authenticators/firebase';
 import { tracked } from '@glimmer/tracking';
+import type Router from '@ember/routing/router-service';
+import { inject as service } from '@ember/service';
 
 export default class PasswordRecoveryController extends Controller {
+  @service router!: Router;
+
   @tracked isConfirmationEmailSend = false;
 
   @tracked email = '';
   @tracked code = '';
   @tracked newPassword = '';
   @tracked error = '';
+
+  enableRecoveryCodeProcessing = false;
 
   get firebase(): FirebaseAuthenticator {
     return getOwner(this).lookup(
@@ -28,7 +34,8 @@ export default class PasswordRecoveryController extends Controller {
   async sendRecoveryLink() {
     this.resetErrors();
     try {
-      await this.firebase.resetPassword(this.email);
+      await this.firebase.resetPassword(this.email.trim());
+      this.router.transitionTo('login');
     } catch (e) {
       this.error = e.message;
     }
@@ -42,5 +49,11 @@ export default class PasswordRecoveryController extends Controller {
     } catch (e) {
       this.error = e.message;
     }
+  }
+
+  @action
+  onSubmit(e: Event) {
+    e.preventDefault();
+    e.stopPropagation();
   }
 }
