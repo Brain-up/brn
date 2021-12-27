ed8fd497bdf7ec2df1060e4e27b3a29dea29f319
Kirill Baurchanu <baurchanu.k@gmail.com>
2021-12-27 21:20:09 +0300

    EPMLABSBRN-981 BE: add column withPictures for subgroup
    - Addressed a code review comment

--- src/main/kotlin/com/epam/brn/dto/request/SubGroupChangeRequest.kt
+++ src/main/kotlin/com/epam/brn/dto/request/SubGroupChangeRequest.kt
@@ -3,5 +3,5 @@ package com.epam.brn.dto.request
 import com.fasterxml.jackson.annotation.JsonCreator
 
 data class SubGroupChangeRequest @JsonCreator internal constructor(
-    var withPictures: Boolean
+    val withPictures: Boolean
 )
