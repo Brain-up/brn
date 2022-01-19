d66c76b0102c04a3957ec55a838507cea2b332a0
Mark Hatvani <mark_hatvani@epam.com>
2022-01-19 12:22:03 +0100

    feature/EPMLABSBRN-552-UI-Add-tab-and-link-to-api-swagger - Add Swagger to menu - Add new modul to show actual SwaggerUI from "/api/v2/api-docs?group=all"

---
+++ frontend-angular/src/app/modules/admin/modules/swagger/swagger.component.ts
@@ -0,0 +1,49 @@
+import SwaggerUI from 'swagger-ui';
+import { AdminApiService } from '@admin/services/api/admin-api.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  AfterViewInit,
+  OnDestroy,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-swagger',
+  templateUrl: './swagger.component.html',
+  styleUrls: ['./swagger.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SwaggerComponent implements AfterViewInit, OnDestroy, OnInit {
+
+  private readonly destroyer$ = new Subject<void>();
+  public swagger;
+
+  constructor(private readonly adminApiService: AdminApiService) {}
+
+  ngOnInit(): void {
+    this.adminApiService
+      .getSwaggerUi()
+      .pipe(takeUntil(this.destroyer$))
+      .subscribe((swagger) => {
+        this.swagger = swagger;
+      });
+  }
+
+  // Use adminApiService, SwaggerUI would GET without the Auth token
+  ngAfterViewInit(): void {
+    SwaggerUI({
+      docExpansion: 'none',
+      domNode: document.getElementById('swagger-ui-item'),
+      layout: 'BaseLayout',
+      spec: JSON.parse(this.swagger),
+    });
+  }
+
+  public ngOnDestroy(): void {
+    this.destroyer$.next();
+    this.destroyer$.complete();
+  }
+}
