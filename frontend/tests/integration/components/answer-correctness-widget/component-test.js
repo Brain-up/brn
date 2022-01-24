bfdaaf6a499fa7ebb47e75ee7338e042f609add7
Aleksandr Kanunnikov <lifeart92@gmail.com>
2022-01-24 11:09:39 +0300

    fix tests

--- frontend/tests/integration/components/answer-correctness-widget/component-test.js
+++ frontend/tests/integration/components/answer-correctness-widget/component-test.js
@@ -7,22 +7,30 @@ module('Integration | Component | answer-correctness-widget', function (hooks) {
   setupRenderingTest(hooks);
 
   test('gets victory image if isCorrect', async function (assert) {
-    await render(
-      hbs`<AnswerCorrectnessWidget @isCorrect={{true}} @maxImagesNumber={{1}}/>`,
-    );
+    await render(hbs`<AnswerCorrectnessWidget @isCorrect={{true}} />`);
 
     assert.dom('[data-test-answer-correctness-widget]').exists();
   });
 
   test('gets regret image if isCorrect is false', async function (assert) {
-    const host = window.location.origin;
+    await render(hbs`<AnswerCorrectnessWidget @isCorrect={{false}} />`);
 
-    await render(
-      hbs`<AnswerCorrectnessWidget @isCorrect={{false}} @maxImagesNumber={{1}} />`,
+    const styleNode = getComputedStyle(
+      document.querySelector('[data-test-answer-correctness-widget]'),
+    ).backgroundImage;
+
+    assert.equal(
+      styleNode.split(',').every((e) => e.includes('regret/regret')),
+      true,
+      'has regret/ in every image path',
     );
-
-    assert.dom('[data-test-answer-correctness-widget]').hasStyle({
-      backgroundImage: `url("${host}/pictures/regret/regret1.jpg"), url("${host}/pictures/regret/regret1.png"), url("${host}/pictures/regret/regret1.jpeg"), url("${host}/pictures/regret/regret1.svg")`,
+    const exts = ['jpg', 'png', 'svg', 'jpeg'].map((e) => `.${e}`);
+    exts.forEach((ext) => {
+      assert.equal(
+        styleNode.includes(ext),
+        true,
+        `has ${ext} image in background`,
+      );
     });
   });
 });
