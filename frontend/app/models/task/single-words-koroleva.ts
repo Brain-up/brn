2b5f6ce7035a64caa714ec9d2d6d37097b71141b
Aleksandr Kanunnikov <lifeart92@gmail.com>
2022-01-24 10:05:21 +0300

    minimal fixes to get koroleva

--- frontend/app/models/task/single-words-koroleva.ts
+++ frontend/app/models/task/single-words-koroleva.ts
@@ -1,5 +1,13 @@
-import Model from '@ember-data/model';
+import SingleWordsModel from './single-simple-words';
 
-export default class TaskSingleWordsKorolevaModel extends Model {
+export default class TaskSingleWordsKorolevaModel extends SingleWordsModel {
+  // exerciseType = 'single-words-koroleva';
+  exerciseType = 'single-simple-words';
+}
 
+// DO NOT DELETE: this is how TypeScript knows how to look up your models.
+declare module 'ember-data/types/registries/model' {
+  export default interface ModelRegistry {
+    'task/single-words-koroleva': SingleWordsModel;
+  }
 }
