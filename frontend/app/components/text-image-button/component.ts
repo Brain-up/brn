8fc54b0bbae642cffc0203df4800f04c3e3281c8
Aleksandr Kanunnikov <lifeart92@gmail.com>
2022-01-29 18:08:41 +0300

    fix tests

--- frontend/app/components/text-image-button/component.ts
+++ frontend/app/components/text-image-button/component.ts
@@ -21,23 +21,15 @@ export default class TextImageButton extends Component<ITextImageButtonArgs> {
     const img = new Image(10, 10);
     img.src = pictureFileUrl;
     img.onerror = async () => {
+      if (this.isDestroying || this.isDestroyed) {
+        return;
+      }
       if (this.shouldLoadSymbol()) {
-        const url = await this.imageLocator.getPictureForWord(this.args.word);
-        if (!url) {
+        const dataURL = await this.imageLocator.getPictureForWordAsDataURL(this.args.word);
+        if (!dataURL) {
           return;
         }
-        const img = new Image();
-        img.crossOrigin = 'anonymous';
-        img.src = url;
-        img.onload = () => {
-          const canvas = document.createElement('canvas');
-          canvas.width = img.width;
-          canvas.height = img.height;
-          const ctx = canvas.getContext('2d');
-          ctx?.drawImage(img, 0, 0);
-          const dataURL = canvas.toDataURL('image/png');
-          element.style.setProperty('--word-picture-url', `url(${dataURL})`);
-        };
+        element.style.setProperty('--word-picture-url', `url(${dataURL})`);
       }
     };
   }
--- frontend/app/services/image-locator.ts
+++ frontend/app/services/image-locator.ts
@@ -3,25 +3,61 @@ import UserDataService from 'brn/services/user-data';
 
 export default class ImageLocatorService extends Service {
   @service('user-data') userData!: UserDataService;
+  async getPictureForWordAsDataURL(word: string): Promise<string|null> {
+    const url = await this.getPictureForWord(word);
+    if (!url) {
+      return null;
+    }
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.crossOrigin = 'anonymous';
+      img.src = url;
+      img.onload = () => {
+        const canvas = document.createElement('canvas');
+        canvas.width = img.width;
+        canvas.height = img.height;
+        const ctx = canvas.getContext('2d');
+        ctx?.drawImage(img, 0, 0);
+        const dataURL = canvas.toDataURL('image/png');
+        resolve(dataURL);
+      };
+      img.onerror = () => reject(null);
+    });
+  }
   async getPictureForWord(word: string): Promise<string | null> {
     const images = await Promise.all([
       this.getImageFromOpensymbols(word),
       this.getImageFromArasaac(word),
     ]);
+    if (this.isDestroying) {
+      return null;
+    }
     const image = images.find((img) => typeof img === 'string');
     if (!image) {
       let trWords = await this.translateWord(word);
+      if (this.isDestroying) {
+        return null;
+      }
       if (!trWords.length) {
         trWords = await this.translateWord(this.fixWord(word));
+        if (this.isDestroying) {
+          return null;
+        }
       }
       for (const trWord of trWords) {
         const result =  await this.getImageFromArasaac(trWord, 'en');
+        if (this.isDestroying) {
+          return null;
+        }
         if (result) {
           return result;
         }
       }
       for (const trWord of trWords) {
         const result =  await this.getImageFromOpensymbols(trWord, 'en');
+        if (this.isDestroying) {
+          return null;
+        }
         if (result) {
           return result;
         }
@@ -38,6 +74,9 @@ export default class ImageLocatorService extends Service {
       const key = 'dict.1.1.20220129T132020Z.f45a308545a3656b.25a8dceae9820a28d7c8a0d14d464c0bb47953b5';
       const lang = encodeURIComponent(`${from}-${to}`);
       const request = await fetch(`https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=${key}&lang=${lang}&text=${encodeURIComponent(word)}&flags=4`);
+      if (this.isDestroying) {
+        return [];
+      }
       const data = await request.json();
       return data.def[0].tr.map((e: any) => e.text);
     } catch (e) {
@@ -75,7 +114,7 @@ export default class ImageLocatorService extends Service {
       : fixedWord;
     return fixedWord;
   }
-  async getImageFromArasaac(word: string, lang = this.userData.activeLocale.split('-')[0]) {
+  private async getImageFromArasaac(word: string, lang = this.userData.activeLocaleShort) {
     try {
       let symbols2 = await fetch(
         'https://api.arasaac.org/api/pictograms/' +
@@ -83,8 +122,13 @@ export default class ImageLocatorService extends Service {
           '/bestsearch/' +
           encodeURIComponent(word),
       );
+      if (this.isDestroying) {
+        return null;
+      }
       let data2 = await symbols2.json();
-
+      if (this.isDestroying) {
+        return null;
+      }
       if (!data2.length) {
         const fixedWord = this.fixWord(word);
         symbols2 = await fetch(
@@ -93,6 +137,9 @@ export default class ImageLocatorService extends Service {
             '/search/' +
             encodeURIComponent(fixedWord),
         );
+        if (this.isDestroying) {
+          return null;
+        }
         data2 = await symbols2.json();
       }
 
@@ -103,7 +150,13 @@ export default class ImageLocatorService extends Service {
             '/search/' +
             encodeURIComponent(word),
         );
+        if (this.isDestroying) {
+          return null;
+        }
         data2 = await symbols2.json();
+        if (this.isDestroying) {
+          return null;
+        }
       }
 
       if (data2.length) {
@@ -115,7 +168,7 @@ export default class ImageLocatorService extends Service {
       return null;
     }
   }
-  async getImageFromOpensymbols(word: string, lang = this.userData.activeLocale.split('-')[0]) {
+  private async getImageFromOpensymbols(word: string, lang = this.userData.activeLocaleShort) {
     try {
       const symbols = await fetch(
         'https://www.opensymbols.org/api/v1/symbols/search?q=' +
@@ -123,6 +176,9 @@ export default class ImageLocatorService extends Service {
           '&locale=' +
           encodeURIComponent(lang),
       );
+      if (this.isDestroying) {
+        return null;
+      }
       const data = await symbols.json();
       if (data.length) {
         return data[0].image_url;
