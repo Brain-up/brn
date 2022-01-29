7dc88c2d6795f6f04267d2288fdd08dd357ddf4b
Aleksandr Kanunnikov <lifeart92@gmail.com>
2022-01-29 11:19:08 +0300

    improve image loading

--- frontend/app/utils/file-url.js
+++ frontend/app/utils/file-url.js
@@ -2,6 +2,9 @@ export function urlForImage(fileUrl) {
   if (fileUrl === null) {
     return null;
   }
+  if (fileUrl.startsWith('http')) {
+    return fileUrl;
+  }
   return `/${fileUrl}`;
 }
 export function urlForAudio(fileUrl) {
