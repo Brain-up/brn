890ea79de79fed7f92c333b3483a7f766a6939d7
Aleksandr Kanunnikov <lifeart92@gmail.com>
2022-02-11 18:05:20 +0300

    ci fix

--- frontend-angular/src/app/modules/admin/modules/swagger/swagger.component.ts
+++ frontend-angular/src/app/modules/admin/modules/swagger/swagger.component.ts
@@ -18,31 +18,34 @@ import {
 })
 export class SwaggerComponent implements AfterViewInit, OnDestroy, OnInit {
   private readonly destroyer$ = new Subject<void>();
-  public swagger;
-
   constructor(private readonly adminApiService: AdminApiService) {}
 
-  async ngOnInit(): Promise<void> {
-    await this.adminApiService
+  ngOnInit(): void {
+    this.adminApiService
       .getSwaggerUi()
       .pipe(takeUntil(this.destroyer$))
       .subscribe((swagger) => {
         this.swagger = swagger;
-      }).toPromise();
+      });
   }
 
-  // Use adminApiService, SwaggerUI would GET without the Auth token
-  ngAfterViewInit(): void {
-    SwaggerUI({
+  _swagger: any = undefined;
+
+  set swagger(value: string) {
+    this._swagger = SwaggerUI({
       docExpansion: 'none',
       domNode: document.getElementById('swagger-ui-item'),
       layout: 'BaseLayout',
-      spec: JSON.parse(this.swagger),
+      spec: JSON.parse(value),
     });
+  }
+  get swagger() {
+    return this._swagger;
   }
 
   public ngOnDestroy(): void {
     this.destroyer$.next();
     this.destroyer$.complete();
+    // have no idea how to destroy swagger-ui
   }
 }
