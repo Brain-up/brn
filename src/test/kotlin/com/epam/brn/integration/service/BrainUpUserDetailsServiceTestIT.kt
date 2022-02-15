3adeeb1031920312283074b8d57f35c015c76091
Igor <igor.banchikov@gmail.com>
2022-02-15 11:33:05 +0300

    Epmlabsbrn 669 increase test coverage (#2180)
    * EPMLABSBRN-669 maked test for GroupLocalePostprocessorImpl
    
    * EPMLABSBRN-669 add docker-local to gitignore
    
    * EPMLABSBRN-669-increase-test-coverage maked BrainUpUserDetailsServiceTestIT
    
    * EPMLABSBRN-669-increase-test-coverage complete LopotkoRecordMappingIteratorProviderTest
    
    * EPMLABSBRN-669 renamed LopotkoProcessorTest for the increasing of coverage
    
    * EPMLABSBRN-669-increase-test-coverage maked SeriesWordsKorolevaRecordProcessorTest.
    
    * EPMLABSBRN-669-increase-test-coverage fixed issue with SeriesWordsKorolevaRecordProcessorTest
    
    * EPMLABSBRN-669-increase-test-coverage maked SeriesWordsKorolevaRecordMappingIteratorProviderTest
    
    * EPMLABSBRN-669-increase-test-coverage add one branch to SeriesWordsKorolevaRecordProcessorTest
    
    * EPMLABSBRN-669 some renaming
    
    Co-authored-by: BIV <VTB4054386@region.vtb.ru>
    Co-authored-by: Elena <elena_moshnikova@epam.com>

---
+++ src/test/kotlin/com/epam/brn/integration/service/BrainUpUserDetailsServiceTestIT.kt
@@ -0,0 +1,91 @@
+package com.epam.brn.integration.service
+
+import com.epam.brn.integration.BaseIT
+import com.epam.brn.model.Gender
+import com.epam.brn.model.UserAccount
+import com.epam.brn.repo.UserAccountRepository
+import com.epam.brn.service.BrainUpUserDetailsService
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import java.util.Objects.nonNull
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+
+internal class BrainUpUserDetailsServiceTestIT : BaseIT() {
+
+    @Autowired
+    private lateinit var brainUpUserDetailsService: BrainUpUserDetailsService
+
+    @Autowired
+    private lateinit var userAccountRepository: UserAccountRepository
+
+    @BeforeEach
+    fun setUp() {
+        val userAccount = UserAccount(
+            id = null,
+            fullName = "testUserFirstName",
+            gender = Gender.MALE.toString(),
+            bornYear = 2000,
+            email = "default@gmail.com",
+            active = true
+        )
+        val userAccount1 = UserAccount(
+            id = null,
+            fullName = "testUserFirstName",
+            gender = Gender.MALE.toString(),
+            bornYear = 2000,
+            email = "default1@gmail.com",
+            active = true
+        )
+        val userAccount2 = UserAccount(
+            id = null,
+            fullName = "testUserFirstName",
+            gender = Gender.MALE.toString(),
+            bornYear = 2000,
+            email = "default2@gmail.com",
+            active = true
+        )
+        userAccountRepository.saveAll(listOf(userAccount, userAccount1, userAccount2))
+    }
+
+    @AfterEach
+    fun deleteAfterTest() {
+        userAccountRepository.deleteAll()
+    }
+
+    @ParameterizedTest(name = "Method with userEmail {0} should return valid UserDetails or throw expected exception {1} with message {2}")
+    @MethodSource("getUserEmailsAndAccountData")
+    fun loadUserByUsername(sourceUserEmail: String, ex: Class<out Exception>?, exMessage: String?) {
+        if (nonNull(ex)) {
+            val actualException = Assertions.assertThrows(ex) {
+                brainUpUserDetailsService.loadUserByUsername(sourceUserEmail)
+            }
+            assertEquals(actualException.message, exMessage)
+            return
+        }
+        val userDetails: UserDetails = brainUpUserDetailsService.loadUserByUsername(sourceUserEmail)
+        assertNotNull(userDetails)
+        assertEquals(userDetails.username, sourceUserEmail)
+    }
+
+    companion object {
+        @JvmStatic
+        private fun getUserEmailsAndAccountData() = listOf(
+            Arguments.of("default@gmail.com", null, null),
+            Arguments.of("default1@gmail.com", null, null),
+            Arguments.of("default2@gmail.com", null, null),
+            Arguments.of(
+                "missed@g.com",
+                UsernameNotFoundException::class.java,
+                "User with email: missed@g.com doesn't exist"
+            )
+        )
+    }
+}
