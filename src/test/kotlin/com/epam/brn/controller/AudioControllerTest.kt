8f1a5c124ffa3f8808e2795bf97193dccd39faf2
Elena Moshnikova <elena_moshnikova@epam.com>
2022-02-20 11:08:16 +0300

    998 make slower

--- src/test/kotlin/com/epam/brn/controller/AudioControllerTest.kt
+++ src/test/kotlin/com/epam/brn/controller/AudioControllerTest.kt
@@ -8,10 +8,9 @@ import io.mockk.junit5.MockKExtension
 import io.mockk.verify
 import org.apache.http.HttpStatus
 import org.junit.jupiter.api.Test
-
 import org.junit.jupiter.api.extension.ExtendWith
-import java.io.InputStream
 import java.io.ByteArrayInputStream
+import java.io.InputStream
 import kotlin.test.assertEquals
 
 @ExtendWith(MockKExtension::class)
@@ -26,21 +25,17 @@ internal class AudioControllerTest {
 
     @Test
     fun `should get audio byte array`() {
-
         // GIVEN
         val text = "Testing_text"
-
         val locale = "locale"
-
         val stream: InputStream = ByteArrayInputStream(byteArrayOf(10, 20, 30, 40, 50))
-
-        every { yandex.generateAudioOggFileWithValidation(text, locale) } returns stream
+        every { yandex.generateAudioOggFileWithValidation(text, locale, "", "1") } returns stream
 
         // WHEN
-        val audioByteArray = controller.getAudioByteArray(text, locale)
+        val audioByteArray = controller.getAudioByteArray(text, locale, "", "1")
 
         // THEN
         assertEquals(HttpStatus.SC_OK, audioByteArray.statusCode.value())
-        verify(exactly = 1) { yandex.generateAudioOggFileWithValidation(text, locale) }
+        verify(exactly = 1) { yandex.generateAudioOggFileWithValidation(text, locale, "", "1") }
     }
 }
