83cf26abe6dbb834d2233ea15d9c0888d922e775
Alex Kanunnikov <lifeart92@gmail.com>
2022-04-25 11:40:10 +0300

    EPMLABSBRN-656 (#2212)
    validation
    
    fix bdate error
    
    allow to change gender

--- frontend/app/routes/profile/index.ts
+++ frontend/app/routes/profile/index.ts
@@ -1,3 +1,9 @@
 import Route from '@ember/routing/route';
-
-export default class ProfileRoute extends Route {}
+import { inject as service } from '@ember/service';
+import NetworkService from 'brn/services/network';
+export default class ProfileRoute extends Route {
+  @service('network') declare network: NetworkService;
+  async model() {
+    await this.network.loadCurrentUser();
+  }
+}
