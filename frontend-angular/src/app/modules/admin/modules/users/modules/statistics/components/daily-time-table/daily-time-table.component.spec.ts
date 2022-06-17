6d1cb911625b445b67bea905dc44cc5980371885
Koylubaev Nikita <koylubaevnt@gmail.com>
2022-06-17 22:48:26 +0500

    Fix test error

--- frontend-angular/src/app/modules/admin/modules/users/modules/statistics/components/daily-time-table/daily-time-table.component.spec.ts
+++ frontend-angular/src/app/modules/admin/modules/users/modules/statistics/components/daily-time-table/daily-time-table.component.spec.ts
@@ -1,25 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DailyTimeTableComponent } from './daily-time-table.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { AdminApiServiceFake } from '@admin/services/api/admin-api.service.fake';
+import { AdminApiService } from '@admin/services/api/admin-api.service';
 
 describe('DailyTimeTableComponent', () => {
+  const responseDelayInMs = 0;
   let component: DailyTimeTableComponent;
   let fixture: ComponentFixture<DailyTimeTableComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ DailyTimeTableComponent ]
-    })
-    .compileComponents();
-  });
-
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [DailyTimeTableComponent],
+      imports: [TranslateModule.forRoot()],
+      providers: [
+        {
+          provide: AdminApiService,
+          useFactory: () => new AdminApiServiceFake({responseDelayInMs}),
+        },
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
+    });
     fixture = TestBed.createComponent(DailyTimeTableComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
