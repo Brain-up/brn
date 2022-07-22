ae72c00106bcd1ab83a173eabbf3f25c051618db
Andrei_Mironov <mironovandrey972@gmail.com>
2022-07-22 15:08:21 +0200

    #2241 BE: prepare correct url for task picture

--- src/main/kotlin/com/epam/brn/service/UrlConversionService.kt
+++ src/main/kotlin/com/epam/brn/service/UrlConversionService.kt
@@ -1,23 +1,42 @@
 package com.epam.brn.service
 
-import com.epam.brn.config.AwsConfig
+import com.epam.brn.service.cloud.CloudService
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
-class UrlConversionService(private val awsConfig: AwsConfig) {
+class UrlConversionService(private val cloudService: CloudService) {
     @Value(value = "\${aws.folderForThemePictures}")
     private lateinit var folderForThemePictures: String
+
+    @Value("\${brn.resources.default-pictures.path}")
+    lateinit var defaultPicturesPath: String
+
+    @Value("\${brn.resources.unverified-pictures.path:}")
+    lateinit var unverifiedPicturesPath: String
 
     fun makeUrlForNoise(noiseUrl: String?): String {
         if (noiseUrl.isNullOrEmpty())
             return ""
-        return awsConfig.baseFileUrl + noiseUrl
+        return cloudService.baseFileUrl() + noiseUrl
     }
 
     fun makeUrlForSubGroupPicture(subGroupCode: String): String =
-        awsConfig.baseFileUrl + folderForThemePictures + "/" + subGroupCode + ".svg"
+        cloudService.baseFileUrl() + folderForThemePictures + "/" + subGroupCode + ".svg"
 
-    fun makeUrlForTaskPicture(pictureTaskUrl: String?): String =
-        awsConfig.baseFileUrl + "/" + pictureTaskUrl?.replace("jpg", "png")?.lowercase()
+    fun makeUrlForTaskPicture(pictureTaskUrl: String?): String {
+        val fileName = getFileNameFromPictureUrl(pictureTaskUrl)
+        return if (cloudService.isFileExist(defaultPicturesPath, fileName))
+            createFullPictureTaskUrl(defaultPicturesPath, fileName)
+        else if (cloudService.isFileExist(unverifiedPicturesPath, fileName))
+            createFullPictureTaskUrl(unverifiedPicturesPath, fileName)
+        else
+            ""
+    }
+
+    private fun getFileNameFromPictureUrl(pictureTaskUrl: String?): String =
+        pictureTaskUrl?.substring(pictureTaskUrl.lastIndexOf("/") + 1).orEmpty()
+
+    private fun createFullPictureTaskUrl(filePath: String, fileName: String): String =
+        cloudService.baseFileUrl() + "/" + filePath + "/" + fileName.replace("jpg", "png").lowercase()
 }
