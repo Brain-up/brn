2c66609e6b9a505d34cb22cf0d169744f0b7381d
Andrei Mironov <mironovandrey972@gmail.com>
2022-08-02 21:36:22 +0200

    #2241 BE: prepare correct url for task picture (#2264)

--- src/main/kotlin/com/epam/brn/service/UrlConversionService.kt
+++ src/main/kotlin/com/epam/brn/service/UrlConversionService.kt
@@ -1,23 +1,40 @@
 package com.epam.brn.service
 
-import com.epam.brn.config.AwsConfig
+import com.epam.brn.service.cloud.CloudService
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
-class UrlConversionService(private val awsConfig: AwsConfig) {
-    @Value(value = "\${aws.folderForThemePictures}")
+class UrlConversionService(private val cloudService: CloudService) {
+    @Value("\${aws.folderForThemePictures}")
     private lateinit var folderForThemePictures: String
+
+    @Value("\${brn.resources.default-pictures.path}")
+    lateinit var defaultPicturesPath: String
+
+    @Value("\${brn.resources.unverified-pictures.path:}")
+    lateinit var unverifiedPicturesPath: String
+
+    @Value("\${brn.resources.pictures.ext}")
+    lateinit var pictureExtensions: Set<String>
 
     fun makeUrlForNoise(noiseUrl: String?): String {
         if (noiseUrl.isNullOrEmpty())
             return ""
-        return awsConfig.baseFileUrl + noiseUrl
+        return cloudService.baseFileUrl() + noiseUrl
     }
 
     fun makeUrlForSubGroupPicture(subGroupCode: String): String =
-        awsConfig.baseFileUrl + folderForThemePictures + "/" + subGroupCode + ".svg"
+        cloudService.baseFileUrl() + folderForThemePictures + "/" + subGroupCode + ".svg"
 
-    fun makeUrlForTaskPicture(pictureTaskUrl: String?): String =
-        awsConfig.baseFileUrl + "/" + pictureTaskUrl?.replace("jpg", "png")?.lowercase()
+    fun makeUrlForTaskPicture(word: String): String {
+        listOf(defaultPicturesPath, unverifiedPicturesPath).forEach { picturesPath ->
+            pictureExtensions.forEach { ext ->
+                val fileName = "$word.$ext"
+                if (cloudService.isFileExist(picturesPath, fileName))
+                    return cloudService.baseFileUrl() + "/" + cloudService.createFullFileName(picturesPath, fileName)
+            }
+        }
+        return ""
+    }
 }
