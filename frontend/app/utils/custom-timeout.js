bf1e6f895e1d9021e60b6c77998642af25475469
Koylubaev Nikita <koylubaevnt@gmail.com>
2022-08-09 15:26:04 +0500

    Removed some additional parameters, because it's not needed

--- frontend/app/utils/custom-timeout.js
+++ frontend/app/utils/custom-timeout.js
@@ -1,7 +1,7 @@
 import { timeout } from 'ember-concurrency';
 import Ember from 'ember';
 
-export default async function customTimeout(ms = 16, useMsValueForTest = false) {
-  const delay = Ember.testing ? ( useMsValueForTest ? ms : 1): ms;
+export default async function customTimeout(ms = 16) {
+  const delay = Ember.testing ? 1 : ms;
   return await timeout(delay);
 }
