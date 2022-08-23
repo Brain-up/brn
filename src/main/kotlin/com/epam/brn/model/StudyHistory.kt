080544d1f7c49c3e080e47c1d7a4018b4af35e0a
Andrei_Mironov <mironovandrey972@gmail.com>
2022-08-23 19:52:35 +0200

    #2255 BE: renamed spentTime variable to spentTimeInSeconds

--- src/main/kotlin/com/epam/brn/model/StudyHistory.kt
+++ src/main/kotlin/com/epam/brn/model/StudyHistory.kt
@@ -3,16 +3,7 @@ package com.epam.brn.model
 import com.epam.brn.dto.StudyHistoryDto
 import java.time.LocalDateTime
 import java.time.ZoneOffset
-import javax.persistence.Entity
-import javax.persistence.FetchType
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
-import javax.persistence.Index
-import javax.persistence.JoinColumn
-import javax.persistence.ManyToOne
-import javax.persistence.Table
-import javax.persistence.UniqueConstraint
+import javax.persistence.*
 
 @Entity
 @Table(
@@ -33,7 +24,8 @@ data class StudyHistory(
     var exercise: Exercise,
     var startTime: LocalDateTime,
     var endTime: LocalDateTime? = null,
-    var spentTime: Long? = endTime?.toInstant(ZoneOffset.UTC)?.epochSecond
+    @Column(name = "spent_time")
+    var spentTimeInSeconds: Long? = endTime?.toInstant(ZoneOffset.UTC)?.epochSecond
         ?.minus(startTime.toInstant(ZoneOffset.UTC).epochSecond),
     var executionSeconds: Int,
     var tasksCount: Short,
@@ -44,7 +36,7 @@ data class StudyHistory(
 
 ) {
     override fun toString() =
-        "StudyHistory(id=$id, userAccount=$userAccount, exercise=$exercise, startTime=$startTime, endTime=$endTime, spentTime=$spentTime, tasksCount=$tasksCount, wrongAnswers=$wrongAnswers)"
+        "StudyHistory(id=$id, userAccount=$userAccount, exercise=$exercise, startTime=$startTime, endTime=$endTime, spentTime=$spentTimeInSeconds, tasksCount=$tasksCount, wrongAnswers=$wrongAnswers)"
 
     fun toDto() = StudyHistoryDto(
         id = this.id,
