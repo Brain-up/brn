d15fd75e38955ab851b070fe0b80bbda39030b5c
Maksim_Rudz <rudzmaksim@gmail.com>
2022-09-22 20:44:05 +0200

    2295: fix SQL script

--- src/main/resources/db/migration/V220220923_2295.sql
+++ src/main/resources/db/migration/V220220923_2295.sql
@@ -1,1 +1,4 @@
-INSERT INTO user_authorities(user_id, authority_id) VALUES (1, 3), (1, 4);
+INSERT INTO user_authorities(user_id, authority_id)
+SELECT u.id, a.id FROM user_account u JOIN authority a ON a.id != 2
+WHERE EXISTS (SELECT 1 FROM user_authorities ua WHERE ua.user_id = u.id AND ua.authority_id = 2)
+  AND NOT EXISTS (SELECT 1 FROM user_authorities ua WHERE ua.authority_id = a.id AND ua.user_id = u.id)
