46e769bd559c7839f0b73187c0597d8e4ec7a442
Mikhail Moshnikov <62326146+Mikleprog@users.noreply.github.com>
2022-10-05 13:50:00 +0300

    [UI Angular] Show spentTime in readable view (#2325)
    Co-authored-by: Mikhail.Moshnikov <Mikhail.Moshnikov@wolterskluwer.com>

---
+++ frontend-angular/src/app/modules/shared/pipes/duration-transform.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'durationTransform',
+})
+export class DurationTransformPipe implements PipeTransform {
+  transform(spentTime: number): string {
+    if (spentTime) {
+      const fullSeconds = spentTime / 1e9;
+      let hours = Math.floor(fullSeconds / 3600).toString();
+      let minutes = Math.floor(fullSeconds % 3600 / 60).toString();
+      let seconds = (fullSeconds % 60).toString();
+      hours = + hours < 10 ? '0' + hours : hours;
+      minutes = + minutes < 10 ? '0' + minutes : minutes;
+      seconds = + seconds < 10 ? '0' + seconds : seconds;
+
+      return `${hours}h:${minutes}m:${seconds}s`;
+    }
+    return '0';
+  }
+}
--- frontend-angular/src/app/modules/shared/pipes/pipes.module.ts
+++ frontend-angular/src/app/modules/shared/pipes/pipes.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { ShortNamePipe } from './short-name.pipe';
+import { DurationTransformPipe } from './duration-transform.pipe';
 
 @NgModule({
-  declarations: [ShortNamePipe],
-  exports: [ShortNamePipe],
+  declarations: [ShortNamePipe, DurationTransformPipe],
+  exports: [ShortNamePipe, DurationTransformPipe],
 })
 export class PipesModule {}
