d80822d34f9cb44782c4e7aed13912418c0d3ca3
Elena Moshnikova <elena_moshnikova@epam.com>
2022-10-05 14:51:07 +0300

    000 delete for now - will be added in next task

--- src/main/resources/db/migration/V220220923_2295.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO user_authorities(user_id, authority_id)
-SELECT u.id, a.id FROM user_account u JOIN authority a ON a.id != 2
-WHERE EXISTS (SELECT 1 FROM user_authorities ua WHERE ua.user_id = u.id AND ua.authority_id = 2)
-  AND NOT EXISTS (SELECT 1 FROM user_authorities ua WHERE ua.authority_id = a.id AND ua.user_id = u.id)
