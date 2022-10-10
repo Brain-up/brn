e1782031e67baf198bfa28331595889b65faa582
Koylubaev Nikita <koylubaevnt@gmail.com>
2022-10-10 18:38:38 +0500

    2281 [BE] Design and Implement getting contributors from our github project, save them into our DB, prepare api for getting them to our front
    * Deleted unused property API_GITHUB_USERNAME

--- src/main/kotlin/com/epam/brn/webclient/property/GitHubApiClientProperty.kt
+++ src/main/kotlin/com/epam/brn/webclient/property/GitHubApiClientProperty.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConstructorBinding
 
 @ConstructorBinding
 @ConfigurationProperties(prefix = "github.api")
-data class GitHubApiClientProperty(val username: String, val token: String, val url: GitHubApiUrl) {
+data class GitHubApiClientProperty(val token: String, val url: GitHubApiUrl) {
     data class GitHubApiUrl(val base: String, val path: GitHubApiPath) {
         data class GitHubApiPath(val contributors: String, val users: String)
     }
