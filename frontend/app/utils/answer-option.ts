20f5f269eb564d8e9b7ab50434b97db925405a3e
Alex Kanunnikov <lifeart92@gmail.com>
2022-11-03 20:49:17 +0300

    Word Pronounce (#2361)

--- frontend/app/utils/answer-option.ts
+++ frontend/app/utils/answer-option.ts
@@ -3,6 +3,7 @@ export default class AnswerOption {
   audioFileUrl: string | null = null;
   id: string | null = null;
   word = '';
+  wordPronounce = '';
   pictureFileUrl: string | null = null;
   columnNumber: number;
   constructor(
@@ -12,18 +13,21 @@ export default class AnswerOption {
       word,
       columnNumber,
       pictureFileUrl,
+      wordPronounce,
     }: {
       id: string;
       audioFileUrl?: null | string;
       columnNumber?: number;
       word: string;
       pictureFileUrl?: null | string;
-    } = { id: '42', word: '', columnNumber: 0 },
+      wordPronounce?: string;
+    } = { id: '42', word: '', columnNumber: 0, wordPronounce: undefined },
   ) {
     // this.audioFileUrl = '/audio/no_noise/%D0%B1%D0%B0%D0%BB.mp3';
     this.audioFileUrl = audioFileUrl ? urlForAudio(audioFileUrl) : null;
     this.columnNumber = columnNumber ?? 0;
     this.id = id;
+    this.wordPronounce = wordPronounce ?? word;
     this.word = word;
     this.pictureFileUrl = pictureFileUrl ? urlForImage(pictureFileUrl) : null;
     // this is for eslint, due to files bugfix
