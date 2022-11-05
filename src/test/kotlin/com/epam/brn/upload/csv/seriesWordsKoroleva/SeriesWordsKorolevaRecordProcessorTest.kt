e70cf964563dd62f8459812aed66412d87bb32eb
Anatol Voluznev <a.piskarev@lipt-soft.ru>
2022-11-05 18:15:49 +0300

    2311 refactor entities

--- src/test/kotlin/com/epam/brn/upload/csv/seriesWordsKoroleva/SeriesWordsKorolevaRecordProcessorTest.kt
+++ src/test/kotlin/com/epam/brn/upload/csv/seriesWordsKoroleva/SeriesWordsKorolevaRecordProcessorTest.kt
@@ -26,7 +26,6 @@ import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.kotlin.argThat
 import org.springframework.test.util.ReflectionTestUtils
 import java.util.Optional
 
