cc51a4e84e47f22f6bf11bd546e092d94ef26c61
Elena Moshnikova <elena_moshnikova@epam.com>
2022-11-16 22:24:07 +0300

    2392 implement exerciseMechanism on Task level

--- src/main/kotlin/com/epam/brn/enums/ExerciseMechanism.kt
+++ src/main/kotlin/com/epam/brn/enums/ExerciseMechanism.kt
@@ -1,8 +1,8 @@
 package com.epam.brn.enums
 
 enum class ExerciseMechanism {
-    WORDS,
-    SENTENCES,
-    MATRIX,
+    WORDS, // show words in random places, play one after another n*2+1 times
+    SENTENCES, // show sentences by exercise template, play whole sentence by random words in columns
+    MATRIX, // show sentences by exercise template, play sentence words independently by random words in columns
     SIGNALS;
 }
