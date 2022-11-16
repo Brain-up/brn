72fe2b81b80e8efa75ed6ba012ca9b0be2a7ad8f
Elena Moshnikova <elena_moshnikova@epam.com>
2022-11-16 14:49:43 +0300

    2384 add ex

--- src/test/kotlin/com/epam/brn/integration/CsvLoadingTestIT.kt
+++ src/test/kotlin/com/epam/brn/integration/CsvLoadingTestIT.kt
@@ -87,7 +87,7 @@ class CsvLoadingTestIT : BaseIT() {
         audiometryTaskRepository.findAll() shouldHaveSize 24
         exerciseGroupRepository.findAll() shouldHaveSize 4
         seriesRepository.findAll() shouldHaveSize 17
-        subGroupRepository.findAll() shouldHaveSize 127
+        subGroupRepository.findAll() shouldHaveSize 128
 //        exerciseRepository.findAll() shouldHaveSize 188
 //        taskRepository.findAll() shouldHaveSize 188
 //        resourceRepository.findAll() shouldHaveSize 881
