c04eeb016c4080140157a5065cc7dd1913eb76cf
Elena Moshnikova <elena_moshnikova@epam.com>
2022-11-23 11:31:13 +0300

    2369 use async in prepare pictures for task flow

--- src/test/kotlin/com/epam/brn/service/OptimizationTest.kt
+++ src/test/kotlin/com/epam/brn/service/OptimizationTest.kt
@@ -53,7 +53,7 @@ internal class OptimizationTest {
 
         // THEN
         println("Completed in time=$time ms")
-        time shouldBeInRange 1000L..1400L
+        time shouldBeInRange 1000L..2000L
     }
 
     @Test
