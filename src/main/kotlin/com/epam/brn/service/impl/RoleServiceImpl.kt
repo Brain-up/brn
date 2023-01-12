ea0ce7707f2ee561a7829b834fceb50d543134e9
Elena Moshnikova <elena_moshnikova@epam.com>
2023-01-12 22:29:08 +0300

    2438 all exercises are open for spechialist

--- src/main/kotlin/com/epam/brn/service/impl/RoleServiceImpl.kt
+++ src/main/kotlin/com/epam/brn/service/impl/RoleServiceImpl.kt
@@ -6,36 +6,34 @@ import com.epam.brn.exception.EntityNotFoundException
 import com.epam.brn.model.Role
 import com.epam.brn.repo.RoleRepository
 import com.epam.brn.service.RoleService
-import org.apache.logging.log4j.kotlin.logger
 import org.springframework.stereotype.Service
 import org.springframework.web.context.request.RequestContextHolder
-
 import org.springframework.web.context.request.ServletRequestAttributes
 
 @Service
 class RoleServiceImpl(private val roleRepository: RoleRepository) : RoleService {
 
-    private val log = logger()
+    override fun findById(id: Long): Role =
+        roleRepository
+            .findById(id)
+            .orElseThrow { EntityNotFoundException("Role with id = $id is not found") }
 
-    override fun findById(id: Long): Role {
-        log.debug("getting the role with roleId=$id")
-        return roleRepository.findById(id).orElseThrow { EntityNotFoundException("Role with id = $id is not found") }
-    }
-
-    override fun findByName(name: String): Role {
-        log.debug("getting the role with roleName=$name")
-        return roleRepository.findByName(name)
+    override fun findByName(name: String): Role =
+        roleRepository
+            .findByName(name)
             ?: throw EntityNotFoundException("Role with name = $name is not found")
-    }
 
-    override fun save(role: Role) = roleRepository.save(role)
+    override fun save(role: Role) =
+        roleRepository.save(role)
 
     override fun isCurrentUserAdmin(): Boolean {
         val request = (RequestContextHolder.currentRequestAttributes() as ServletRequestAttributes).request
         return request.isUserInRole(BrnRole.ADMIN)
     }
 
-    override fun isUserHasRole(user: UserAccountDto, role: String) = user.roles?.contains(role) ?: false
+    override fun isUserHasRole(user: UserAccountDto, role: String): Boolean =
+        user.roles.contains(role)
 
-    override fun findAll(): List<Role> = roleRepository.findAll()
+    override fun findAll(): List<Role> =
+        roleRepository.findAll()
 }
