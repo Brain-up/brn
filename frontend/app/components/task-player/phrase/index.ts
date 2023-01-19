7795915470cb744258da3476a39f0bd082321a39
Alex Kanunnikov <lifeart92@gmail.com>
2023-01-19 19:49:16 +0300

    remove extra components

--- frontend/app/components/task-player/phrase/index.ts
+++
@@ -1,3 +0,0 @@
-import Component from 'brn/components/task-player/single-simple-words/component';
-
-export default class TaskPlayerPhraseComponent extends Component {}
--- frontend/tests/integration/components/task-player/single-words/test-support/helpers.js
+++
@@ -1,5 +0,0 @@
-import { click } from '@ember/test-helpers';
-
-export async function chooseAnswer(option) {
-  await click(`[data-test-task-answer-option="${option}"]`);
-}
