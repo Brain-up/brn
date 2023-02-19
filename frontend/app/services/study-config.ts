b9556401e0cbb7e9c73ceb964c47df3262596157
Alex Kanunnikov <lifeart92@gmail.com>
2023-02-19 18:44:03 +0300

    2462 - opt-out-image display from task model (#2480)

--- frontend/app/services/study-config.ts
+++ frontend/app/services/study-config.ts
@@ -1,13 +1,32 @@
 import Service from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { getOwner } from '@ember/application';
+import Task from 'brn/models/task';
+
 export default class StudyConfigService extends Service {
     @tracked
-    showImages = true;
+    _showImages = true;
+
+    get task(): Task | undefined {
+      return getOwner(this).lookup('controller:group.series.subgroup.exercise.task').model;
+    }
+
+    get allowImagesInTask() {
+      return this.task?.shouldBeWithPictures ?? false;
+    }
+
+    get showImageToggler() {
+      return this.allowImagesInTask;
+    }
+
+    get showImages() {
+      return this._showImages && this.allowImagesInTask;
+    }
 
     @action
     toggleImageVisibility() {
-        this.showImages = !this.showImages;
+      this._showImages = !this._showImages;
     }
 }
 
