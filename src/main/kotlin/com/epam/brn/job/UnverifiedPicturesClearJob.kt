e5126becb320d7363f526a48d57cef7c02184603
Elena Moshnikova <brainupproject@yandex.ru>
2023-06-02 22:58:44 +0300

    000 fix : switch off job, as now it works not correctly - it removes folder unverified (#2507)
    * switch off job,
    as now it works not correctly - it removes folder unverified
    
    * switch off job,
    as now it works not correctly - it removes folder unverified
    
    ---------
    
    Co-authored-by: Elena Moshnikova <elena_moshnikova@epam.com>

--- src/main/kotlin/com/epam/brn/job/UnverifiedPicturesClearJob.kt
+++ src/main/kotlin/com/epam/brn/job/UnverifiedPicturesClearJob.kt
@@ -3,7 +3,6 @@ package com.epam.brn.job
 import com.epam.brn.service.cloud.CloudService
 import org.apache.logging.log4j.kotlin.logger
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Component
@@ -16,14 +15,15 @@ class UnverifiedPicturesClearJob(private val cloudService: CloudService) {
 
     private val log = logger()
 
-    @Scheduled(cron = "\${brn.resources.unverified-pictures.clean-job.cron}")
+    // todo: fix it as now it removes folder unverified
+    // @Scheduled(cron = "\${brn.resources.unverified-pictures.clean-job.cron}")
     fun clearUnusedPictures() {
         val defaultFolderPictures = cloudService.getFileNames(defaultPicturesPath)
         val unverifiedFolderPictures = cloudService.getFileNames(unverifiedPicturesPath)
 
-        val fileNamesToDelete = defaultFolderPictures.intersect(unverifiedFolderPictures).map {
-            unverifiedPicturesPath.plus(it)
-        }
+        val fileNamesToDelete = defaultFolderPictures
+            .intersect(unverifiedFolderPictures)
+            .map { unverifiedPicturesPath.plus(it) }
 
         cloudService.deleteFiles(fileNamesToDelete)
         log.info("Files are deleted from \"$unverifiedPicturesPath\"")
