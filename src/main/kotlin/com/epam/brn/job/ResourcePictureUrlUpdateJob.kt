c45978c5139b43469c08ee6d869483ee96b34040
Elena Moshnikova <brainupproject@yandex.ru>
2023-06-07 16:03:02 +0300

    2508  fix and switch on job which clear unused pictures, add logs for updating pictures urls job (#2509)
    * 2508: fix and switch on job which clear unused pictures, add logs

--- src/main/kotlin/com/epam/brn/job/ResourcePictureUrlUpdateJob.kt
+++ src/main/kotlin/com/epam/brn/job/ResourcePictureUrlUpdateJob.kt
@@ -34,23 +34,27 @@ class ResourcePictureUrlUpdateJob(
                 val defaultFolderPictures: Map<String, String> = cloudService.getFilePathMap(defaultPicturesPath)
                 val unverifiedFolderPictures: Map<String, String> = cloudService.getFilePathMap(unverifiedPicturesPath)
 
+                response.inDefaultFolderPicturesCount = defaultFolderPictures.size
+                response.inUnverifiedFolderPicturesCount = unverifiedFolderPictures.size
+
                 val resources = resourceService.findAll()
                 for (resource in resources) {
                     var fileUrl = String()
                     if (defaultFolderPictures.containsKey(resource.word)) {
                         fileUrl = defaultFolderPictures[resource.word].toString()
-                        response.inDefaultFolderPicturesCount++
+                        response.withCorrectDefaultUrlResources++
                     } else if (unverifiedFolderPictures.containsKey(resource.word)) {
                         fileUrl = unverifiedFolderPictures[resource.word].toString()
-                        response.inUnverifiedFolderPicturesCount++
+                        response.withUnverifiedUrlResources++
                     } else {
                         log.debug("No picture for ${resource.word} found")
-                        response.notFoundPictureForWordCount++
+                        response.withoutPicturesResources++
                     }
 
                     val shouldUpdateUrl = fileUrl.isNotEmpty() && fileUrl != resource.pictureFileUrl
                     val shouldCleanUrl = fileUrl.isEmpty() && !resource.pictureFileUrl.isNullOrEmpty()
                     if (shouldUpdateUrl || shouldCleanUrl) {
+                        response.updatedResources++
                         resource.pictureFileUrl = fileUrl
                         updatedResources.add(resource)
                     }
@@ -59,17 +63,20 @@ class ResourcePictureUrlUpdateJob(
                     resourceService.saveAll(updatedResources)
             } catch (e: Exception) {
                 response.success = false
-                response.errorMessage = e.message
+                response.errorMessage = e.message.orEmpty()
                 log.error("Error updating picture URLs for resources", e)
             }
         }
         val logStatement =
             """
                 End Job: update picture URLs
-                Success: ${response.success}
-                Used default URLs count: ${response.inDefaultFolderPicturesCount}
-                Used unverified URLs count: ${response.inUnverifiedFolderPicturesCount}
-                Not found count: ${response.notFoundPictureForWordCount}
+                Success: ${response.success}, message: ${response.errorMessage}
+                Pictures in Default folder: ${response.inDefaultFolderPicturesCount}
+                Pictures in Unverified folder: ${response.inUnverifiedFolderPicturesCount}
+                Used default URLs count: ${response.withCorrectDefaultUrlResources}
+                Used unverified URLs count: ${response.withUnverifiedUrlResources}
+                Without pictures resources: ${response.withoutPicturesResources}
+                Updated resources: ${response.updatedResources}
                 Execution Time: ${executionTime / 1000}s
             """
         log.info(logStatement)
@@ -82,7 +89,10 @@ data class ResourcePictureUrlUpdateJobResponse(
     var success: Boolean = true,
     var inDefaultFolderPicturesCount: Int = 0,
     var inUnverifiedFolderPicturesCount: Int = 0,
-    var notFoundPictureForWordCount: Int = 0,
+    var withCorrectDefaultUrlResources: Int = 0,
+    var withUnverifiedUrlResources: Int = 0,
+    var withoutPicturesResources: Int = 0,
+    var updatedResources: Int = 0,
     var executionTime: Long = 0,
-    var errorMessage: String? = null
+    var errorMessage: String = "No errors!"
 )
--- src/main/kotlin/com/epam/brn/job/UnverifiedPicturesClearJob.kt
+++ src/main/kotlin/com/epam/brn/job/UnverifiedPicturesClearJob.kt
@@ -3,6 +3,7 @@ package com.epam.brn.job
 import com.epam.brn.service.cloud.CloudService
 import org.apache.logging.log4j.kotlin.logger
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Component
@@ -15,17 +16,20 @@ class UnverifiedPicturesClearJob(private val cloudService: CloudService) {
 
     private val log = logger()
 
-    // todo: fix it as now it removes folder unverified
-    // @Scheduled(cron = "\${brn.resources.unverified-pictures.clean-job.cron}")
+    @Scheduled(cron = "\${brn.resources.unverified-pictures.clean-job.cron}")
     fun clearUnusedPictures() {
-        val defaultFolderPictures = cloudService.getFileNames(defaultPicturesPath)
-        val unverifiedFolderPictures = cloudService.getFileNames(unverifiedPicturesPath)
+        val unverifiedFolderPictures: List<String> = cloudService
+            .getFileNames(unverifiedPicturesPath)
+            .filter { it != "/" }
+        val defaultFolderPictures: List<String> = cloudService
+            .getFileNames(defaultPicturesPath)
+            .filter { it != "/" }
 
         val fileNamesToDelete = defaultFolderPictures
             .intersect(unverifiedFolderPictures)
             .map { unverifiedPicturesPath.plus(it) }
 
         cloudService.deleteFiles(fileNamesToDelete)
-        log.info("Files are deleted from \"$unverifiedPicturesPath\"")
+        log.info("Files ${fileNamesToDelete.size} are deleted from \"$unverifiedPicturesPath\"")
     }
 }
