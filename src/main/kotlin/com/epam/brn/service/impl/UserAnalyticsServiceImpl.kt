8773ac2b5e0ddd6648ac985225c50423b0f7ce5e
Elena_Moshnikova <trunion@yandex.ru>
2024-11-23 22:42:18 +0300

    2647 manage voices: fix

--- src/main/kotlin/com/epam/brn/service/impl/UserAnalyticsServiceImpl.kt
+++ src/main/kotlin/com/epam/brn/service/impl/UserAnalyticsServiceImpl.kt
@@ -80,7 +80,7 @@ class UserAnalyticsServiceImpl(
         if (!listTextExercises.contains(seriesType))
             audioFileMetaData.text = text.replace(" ", ", ")
         val currentUser = userAccountService.getCurrentUser()
-        // todo use choseVoiceForUser(currentUser) after moving to v3
+        // todo use choseVoiceForUser(currentUser) after moving to yandex speechKit v3
         audioFileMetaData.voice = Voice.marina.name
         setSpeedForUser(currentUser, exerciseId, audioFileMetaData)
         return audioFileMetaData
