59c5263ad05441668f8d4b40361d0cb0194bcf8d
Elena_Moshnikova <trunion@yandex.ru>
2024-11-23 12:59:14 +0300

    2647 manage voices

--- src/main/kotlin/com/epam/brn/service/impl/UserAnalyticsServiceImpl.kt
+++ src/main/kotlin/com/epam/brn/service/impl/UserAnalyticsServiceImpl.kt
@@ -4,7 +4,9 @@ import com.epam.brn.dto.AudioFileMetaData
 import com.epam.brn.dto.response.UserWithAnalyticsResponse
 import com.epam.brn.dto.statistics.DayStudyStatistics
 import com.epam.brn.enums.ExerciseType
+import com.epam.brn.enums.Voice
 import com.epam.brn.model.StudyHistory
+import com.epam.brn.model.UserAccount
 import com.epam.brn.repo.ExerciseRepository
 import com.epam.brn.repo.StudyHistoryRepository
 import com.epam.brn.repo.UserAccountRepository
@@ -17,6 +19,7 @@ import com.epam.brn.service.statistics.UserPeriodStatisticsService
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 import java.io.InputStream
+import java.time.LocalDate
 import java.time.LocalTime
 import java.time.temporal.WeekFields
 import java.util.Locale
@@ -72,20 +75,33 @@ class UserAnalyticsServiceImpl(
             )
 
     override fun prepareAudioFileMetaData(exerciseId: Long, audioFileMetaData: AudioFileMetaData): AudioFileMetaData {
-        val currentUserId = userAccountService.getCurrentUserId()
-        val lastExerciseHistory = studyHistoryRepository
-            .findLastByUserAccountIdAndExerciseId(currentUserId, exerciseId)
         val seriesType = ExerciseType.valueOf(exerciseRepository.findTypeByExerciseId(exerciseId))
         val text = audioFileMetaData.text
         if (!listTextExercises.contains(seriesType))
             audioFileMetaData.text = text.replace(" ", ", ")
+        val currentUser = userAccountService.getCurrentUser()
+        audioFileMetaData.voice = choseVoiceForUser(currentUser)
+        setSpeedForUser(currentUser, exerciseId, audioFileMetaData)
+        return audioFileMetaData
+    }
+
+    fun setSpeedForUser(user: UserAccount, exerciseId: Long, audioFileMetaData: AudioFileMetaData) {
+        val lastExerciseHistory = studyHistoryRepository
+            .findLastByUserAccountIdAndExerciseId(user.id!!, exerciseId)
         if (lastExerciseHistory == null)
             audioFileMetaData.setSpeedNormal()
         else if (isDoneBad(lastExerciseHistory))
             audioFileMetaData.setSpeedSlow()
         else if (isDoneWell(lastExerciseHistory))
             audioFileMetaData.setSpeedFaster()
-        return audioFileMetaData
+    }
+
+    fun choseVoiceForUser(user: UserAccount): String {
+        if (user.bornYear == null)
+            return Voice.marina.name
+        val ages = LocalDate.now().year - user.bornYear!!
+        return if (ages < 19) Voice.marina.name
+        else Voice.lera.name
     }
 
     fun isDoneBad(lastHistory: StudyHistory?): Boolean =
