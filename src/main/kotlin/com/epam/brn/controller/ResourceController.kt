5deb6c62f73de2697366adef5b2095854884fae4
Elena Moshnikova <brainupproject@yandex.ru>
2025-03-24 13:02:07 +0400

    BE Add get all resources end-point (#2673)
    * Add get all resources end-point

--- src/main/kotlin/com/epam/brn/controller/ResourceController.kt
+++ src/main/kotlin/com/epam/brn/controller/ResourceController.kt
@@ -1,8 +1,8 @@
 package com.epam.brn.controller
 
-import com.epam.brn.dto.response.ResourceResponse
 import com.epam.brn.dto.request.UpdateResourceDescriptionRequest
 import com.epam.brn.dto.response.BrnResponse
+import com.epam.brn.dto.response.ResourceResponse
 import com.epam.brn.enums.BrnRole
 import com.epam.brn.job.ResourcePictureUrlUpdateJob
 import com.epam.brn.job.ResourcePictureUrlUpdateJobResponse
@@ -11,13 +11,14 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
+import javax.annotation.security.RolesAllowed
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import javax.annotation.security.RolesAllowed
 
 @RestController
 @RequestMapping("/resources")
@@ -27,6 +28,11 @@ class ResourceController(
     val resourceService: ResourceService,
     val resourcePictureUpdateJob: ResourcePictureUrlUpdateJob
 ) {
+
+    @GetMapping
+    @Operation(summary = "Get all resources")
+    fun getResources(): ResponseEntity<List<ResourceResponse>> =
+        ResponseEntity.ok(resourceService.findAll().sortedBy { it.word }.map { it.toResponse() })
 
     @PatchMapping("/{id}")
     @Operation(summary = "Update resource description by resource id")
@@ -39,7 +45,6 @@ class ResourceController(
 
     @PostMapping("/update")
     @Operation(summary = "Update picture URL for all resources")
-    fun updateResourceUrls(): ResponseEntity<ResourcePictureUrlUpdateJobResponse> {
-        return ResponseEntity.ok(resourcePictureUpdateJob.updatePictureUrl())
-    }
+    fun updateResourceUrls(): ResponseEntity<ResourcePictureUrlUpdateJobResponse> =
+        ResponseEntity.ok(resourcePictureUpdateJob.updatePictureUrl())
 }
