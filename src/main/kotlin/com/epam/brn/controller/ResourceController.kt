ec8c1d7b8b96a715cc146621a340fc37d80edfe4
ElenaMoshnikova <brainupspbproject@gmial.com>
2025-03-24 12:50:55 +0400

    fix imports

--- src/main/kotlin/com/epam/brn/controller/ResourceController.kt
+++ src/main/kotlin/com/epam/brn/controller/ResourceController.kt
@@ -11,8 +11,14 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.*
 import javax.annotation.security.RolesAllowed
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/resources")
