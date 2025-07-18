13048ebb1e072d247534cc323ba189f084522397
yevgeniy_medvedev <yevgeniy_medvedev@outlook.com>
2025-07-18 17:07:02 +0200

    Migration fixed by removing duplicates before adding new constraint resource.resource_constrain

--- src/main/resources/db/migration/V220250711_2566.sql
+++ src/main/resources/db/migration/V220250711_2566.sql
@@ -6,6 +6,13 @@ create index if not exists word_word_type_idx on resource (word, word_type);
 
 alter table resource drop constraint resource_constrain;
 
+DELETE FROM resource
+WHERE id NOT IN (
+    SELECT MIN(id)
+    FROM resource
+    GROUP BY word, word_type
+);
+
 alter table resource add constraint resource_constrain unique (word, word_type);
 
 alter table resource drop column audio_file_url;
